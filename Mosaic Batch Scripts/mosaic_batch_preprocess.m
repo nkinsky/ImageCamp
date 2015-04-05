05232a3595aa46a98f886c971e3574812d366499
nkinsky <kinsky@bu.edu>
2015-04-05 16:32:56 -0400

    Bug fixes

--- Mosaic Batch Scripts/mosaic_batch_preprocess.m
+++ Mosaic Batch Scripts/mosaic_batch_preprocess.m
@@ -41,41 +41,44 @@ end
 if num_files == 1
     movie_use = sesh(j).movie;
 elseif num_files >= 1
-    list = mosaic.list('mosaic.Movie');
+    list = mosaic.List('mosaic.Movie');
     for j = 1:num_files
         list.add(sesh(j).movie);
     end
     movie_use = mosaic.concatenateMovies(list);
 end
 
+disp('Check if concatenation has happened properly!: view each movie independently, then check #frames total')
 %% Step 2: Downsample spatially by a factor of 2
+cd(pathname)
+
 movie_use = mosaic.resampleMovie(movie_use, 'spatialReduction', spatial_ds,...
-    'useParallelization', 1);
+    'temporalReduction', 1, 'useParallelization', 1);
 
-movie_ds = mosaic.resampleMovie(movie_use, 'spatialReduction', spatial_ds,...
-    'useParallelization', 1);
+% movie_ds = mosaic.resampleMovie(movie_use, 'spatialReduction', spatial_ds,...
+%     'useParallelization', 1);
 
-%% Step 3 (USER INPUT): Crop movie
-[ rect_crop_mos, rect_crop ] = mos_cropmovie_gui( movie_use);
-
-crop_use = rect_crop_mos/spatial_ds/mic_per_pix; % Adjustment to get crop coordinates correct...
-
-frame_plot = mosaic.extractFrame(movie_use,'frame',100); % Get frame to compare cropping to later for QC check
-
-movie_crop = mosaic.cropMovie(movie_use,crop_use(1), crop_use(4), ...
-    crop_use(3), crop_use(2)); % Crop movie
-
-h = frame_plot.view();
-rectangle('Position', rect_crop)
-h2 = movie_crop.view();
-
-disp('Does this look right?')
-keyboard
-
-close(h,h2);
-
-movie_use = movie_crop;
-clear('movie_crop')
+%% Step 3 (USER INPUT): Crop movie - don't do this for now - screws up later cropping and isn't really worth it!!!
+% [ rect_crop_mos, rect_crop ] = mos_cropmovie_gui( movie_use);
+% 
+% crop_use = rect_crop_mos/spatial_ds/mic_per_pix; % Adjustment to get crop coordinates correct...
+% 
+% frame_plot = mosaic.extractFrame(movie_use,'frame',100); % Get frame to compare cropping to later for QC check
+% 
+% movie_crop = mosaic.cropMovie(movie_use,crop_use(1), crop_use(4), ...
+%     crop_use(3), crop_use(2)); % Crop movie
+% 
+% h = frame_plot.view();
+% rectangle('Position', rect_crop)
+% h2 = movie_crop.view();
+% 
+% disp('Does this look right?')
+% keyboard
+% 
+% close(h,h2);
+% 
+% movie_use = movie_crop;
+% clear('movie_crop')
 %% Step 4 (USER INPUT): Get reference image for motion correction
 
 h2 = movie_use.view();
@@ -138,38 +141,51 @@ disp('Here is your chance to check the motion correction');
 keyboard
 close(hmc)
 
+% NEED TO ADD STEP HERE TO PLOT OUT MOTCORRDATA!!!
+
 %% Step 7; Automagically crop the movie to eliminate the border...or do this manually?
 % Annoying intermediate step - save and reload min_proj
 cd(pathname)
 save_name = 'MotCorrMovie.tif';
 title_label = 'Motion Correction Movie Minimum Projection';
-min_proj_int = mosaic.projectMovie(mot_corr_movie,'Minimum');
+min_proj_int = mosaic.projectMovie(mot_corr_movie,'projectionType','Minimum');
 h = mos_tiff_to_fig(min_proj_int, save_name, title_label );
 
-% Manually for now - get min rectangle
-[ rect_crop_mos, rect_crop ] = mos_cropmovie_gui( mot_corr_movie);
+% % Manually for now - get min rectangle
+% [ rect_crop_mos, rect_crop ] = mos_cropmovie_gui( mot_corr_movie);
+% 
+% ICmovie = mosaic.cropMovie(mot_corr_movie, rect_crop_mos(1), rect_crop_mos(4), ...
+%     rect_crop_mos(3), rect_crop_mos(2)); % Crop movie
+%  % This isn't working for some reason - do in mosaic standalone?
 
-ICmovie = mosaic.cropMovie(movie_corr_movie, rect_crop_mos(1). rect_crop_mos(2), ...
-    rect_crop_mos(3), rect_crop_mos(4)); % Crop movie
+%% Step 7.5: Save MotCorrMovie to be adjusted in Mosaic standalone until I
+% figure out why the crop isn't working...
 
 
-%% Step 8: Create min projection and display
-
-min_proj = mosaic.projectMovie(ICmovie,'Minimum');
-
-% Save it, reload it, display as image, and tag it with the appropriate
-% title - SHOULD write a function for this! Put in while loop so that the
-% final product stays saved...
-
-h = mos_tiff_to_fig(min_proj_int, save_name, title_label );
-
-disp('Here is your chance to re-crop if you are not satisfied')
+disp('Saving MotCorrMovie for final editing')
+mosaic.saveOneObject(mot_corr_movie,'MotCorrMovie.mat');
+disp('Check for MotCorrMovie.mat.  If saved correctly, type "return" to enter Mosaic standalone and do your final editing');
 keyboard
+mosaic.terminate()
+mosaicOpenGui
 
-%% Step 8: Save it!
-
-mosaic.saveOneObject(ICmovie,'ICmovie_comb.mat');
-
-cd(curr_dir)
+% %% Step 8: Create min projection and display
+% 
+% min_proj = mosaic.projectMovie(ICmovie,'projectionType','Minimum');
+% 
+% % Save it, reload it, display as image, and tag it with the appropriate
+% % title - SHOULD write a function for this! Put in while loop so that the
+% % final product stays saved...
+% 
+% h = mos_tiff_to_fig(min_proj_int, save_name, title_label );
+% 
+% disp('Here is your chance to re-crop if you are not satisfied')
+% keyboard
+% 
+% %% Step 8: Save it!
+% 
+% mosaic.saveOneObject(ICmovie,'ICmovie_comb.mat');
+% 
+% cd(curr_dir)
 
 mosaic.terminate()
