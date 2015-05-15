14af3efaea0a43651b21e73c74dd7e3e72a4ebb5
wmau <wmau@bu.edu>
2015-05-15 17:05:39 -0400

    Major additions to multiple session registration
    Added function that finds cells that map onto other cells across
    multiple sessions. Also added new fields for Reg_NeuronIDs.

--- GCamp/multi_image_reg.m
+++ GCamp/multi_image_reg.m
@@ -1,5 +1,5 @@
 function Reg_NeuronIDs = multi_image_reg(base_file, num_sessions, check_neuron_mapping)
-%multi_image_reg(base_file, num_session, check_neuron_mapping)
+%Reg_NeuronIDs = multi_image_reg(base_file, num_session, check_neuron_mapping)
 %
 %   Registers a base file to multiple recording sessions and saves these
 %   registrations in a .mat file claled Reg_NeuronIDs.mat in your base file
@@ -16,7 +16,17 @@ function Reg_NeuronIDs = multi_image_reg(base_file, num_sessions, check_neuron_m
 %       Default is zero for all sessions. 
 %
 %   OUTPUTS: 
-%       Reg_NeuronIDs: struct with the following fields...
+%       Reg_NeuronIDs: 1xN struct (where N is the number of registered
+%       sessions) containing the following fields...
+%
+%           mouse: string containing the name of the mouse. Usually in the
+%           format 'GXX' where X is a digit. 
+%
+%           base_path: path to your base file. 
+%
+%           reg_path: path to the file to which you registered to for that
+%           N. 
+%
 %           neuron_id: 1xn cell where n is the number of neurons in the
 %           first session, and each value is the neuron number in the 2nd
 %           session that maps to the neurons in the 1st session.  An empty
@@ -65,6 +75,10 @@ function Reg_NeuronIDs = multi_image_reg(base_file, num_sessions, check_neuron_m
     temp = regexp(base_file,date_format,'names');
     base_date = [temp.month '-' temp.day '-' temp.year];
     
+    %Get mouse name. 
+    mouse_format = '(?<name>G\d+)';
+    mouse = regexp(base_file,mouse_format,'names'); 
+    
     %Get full file path. 
     reg_file = fullfile(reg_path, reg_filename); 
         
@@ -77,11 +91,12 @@ function Reg_NeuronIDs = multi_image_reg(base_file, num_sessions, check_neuron_m
         [neuron_id, same_neuron, num_bad_cells] = image_register_simple(base_file, reg_file{this_session}, check_neuron_mapping(this_session));
         
         %Build the struct. 
+        Reg_NeuronIDs(this_session).mouse = mouse.name; 
+        Reg_NeuronIDs(this_session).base_path = base_path; 
+        Reg_NeuronIDs(this_session).reg_path = reg_path{this_session};
         Reg_NeuronIDs(this_session).neuron_id = neuron_id;
         Reg_NeuronIDs(this_session).same_neuron = same_neuron;
         Reg_NeuronIDs(this_session).num_bad_cells = num_bad_cells;
-        Reg_NeuronIDs(this_session).base_date = base_date; 
-        Reg_NeuronIDs(this_session).reg_date = reg_date{this_session};
         
         %Save. 
         save ([base_path 'Reg_NeuronIDs.mat'], 'Reg_NeuronIDs'); 
