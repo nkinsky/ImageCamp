2a38523f6645333126fda609c85b0110d9080996
nkinsky <kinsky@bu.edu>
2015-06-05 17:10:45 -0400

    Updates to fix bug in obtaining mouse name.

--- GCamp/helpers/get_name_date_session.m
+++ GCamp/helpers/get_name_date_session.m
@@ -6,7 +6,7 @@ function [ mouse_name, session_date, session_num ] = get_name_date_session( fold
 % Formats for pulling files and sessions
 date_format = '(?<month>\d+)_(?<day>\d+)_(?<year>\d+)';
 session_format = '(?<sesh_number>\d?) -';
-mouse_format = '(?<name>G\d+)';
+mouse_format = '\\(?<name>G\w+\d+)';
 
 % Get date.
 temp = regexp(folder_path,date_format,'names');
@@ -25,7 +25,8 @@ else
 end
 %Get mouse name.
 temp3 = regexp(folder_path,mouse_format,'names');
-mouse_name = temp3.name;
+
+mouse_name = temp3(1).name;
 
 end
 
