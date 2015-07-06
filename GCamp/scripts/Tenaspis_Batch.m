d01ec68bf5c4405b3b95a365810b37e189ce48ea
nkinsky <kinsky@bu.edu>
2015-07-06 19:27:40 -0400

    comments

--- GCamp/scripts/Tenaspis_Batch.m
+++ GCamp/scripts/Tenaspis_Batch.m
@@ -1,6 +1,15 @@
 function [  ] = Tenaspis_Batch( infile,session_struct,no_movie_process )
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
+% Tenaspis_Batch(infile, session_struct, no_movie_process)
+% Runs Tenaspis in batch mode
+% 
+% INPUTS
+%   infile: name of imaging movie to process.  Must be the same for all
+%   sessions
+%   session_struct: structure with 3 fields (.Animal, .Date, and .Session)
+%   that matches structure MD in MakeMouseSessionList
+%   no_movie_process: 0 if you are processing the ICmovie that has been
+%   spatially smoothed only, 1 if you already have the temporal 1st
+%   derivative movie in the directory
 
 % keyboard
 for j = 1: length(session_struct)
