5ff015f1ca678c7e6acce8dedfcfb6c6e25010fd
nkinsky <kinsky@bu.edu>
2015-07-10 16:56:56 -0400

    Progress bar add-in

--- GCamp/helpers/MakeMeanBlobs_batch.m
+++ GCamp/helpers/MakeMeanBlobs_batch.m
@@ -3,6 +3,8 @@ function [ ] = MakeMeanBlobs_batch( session_struct, suppress_output )
 % of MakeMeanBlobs.  suppress_output = 0 updates progress to your screen
 
 for j = 1:length(session_struct)
+    disp(['Processing ' session_struct(j).Date ' Session ' ...
+        num2str(session_struct(j).Session)])
     ChangeDirectory_NK(session_struct(j));
     load('ProcOut.mat','c','cTon','GoodTrs')
     MakeMeanBlobs(c, cTon, GoodTrs, 'suppress_output', suppress_output);
