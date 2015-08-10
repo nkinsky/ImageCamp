ea304e5dc92d8c30909ad8f93aea4a184af5c129
nkinsky <kinsky@bu.edu>
2015-08-10 17:57:00 -0400

    Added in error catching statement for debugging.

--- GCamp/fix_dropped_frames.m
+++ GCamp/fix_dropped_frames.m
@@ -55,7 +55,7 @@ end
 %% 0.5) Check for edge case where the 1st frame is bad - haven't figure this out yet!
 
 for j = 1:num_files
-   if dropped_frames{j}(1) == 1
+   if ~isempty(dropped_frames{j}) && dropped_frames{j}(1) == 1
        error('1st frame is bad - haven''t implemented this edge case fix yet.  Sorry!  At least I acknowledged it')
    end
 end
@@ -69,8 +69,6 @@ end
 % to 1020 and 1105)
 %   - will require writing subfunction(s) that grab these values out of the
 %   text file of xml file - this might be hard for xml files
-
-
 
 end_frame = 0;
 frames_total = zeros(size(frames));
@@ -104,19 +102,21 @@ for j = 1:num_files
     end_frame = end_frame + frames_total(j); % Update the true number frames there should be after each session is concatenated
 end
 
-keyboard
-%% 2) Set up the h5 movie
+% keyboard
+%% 2) Set up the h5 movie - put this in a directory parallel to the original movie
 
 info = h5info(infile,'/Object');
-NumFrames = info.Dataspace.Size(3);
 XDim = info.Dataspace.Size(1);
 YDim = info.Dataspace.Size(2);
 
+size_metadata_use = info.Dataspace.Size;
+size_metadata_use(3) = end_frame;
+
 ext_ind = regexpi(infile,'.h5');
 outfile = [infile(1:ext_ind-1) '_fixed.h5'];
-h5create(outfile,'/Object',info.Dataspace.Size,'ChunkSize',[XDim YDim 1 1],'Datatype','uint16');
+h5create(outfile,'/Object',size_metadata_use,'ChunkSize',[XDim YDim 1 1],'Datatype','uint16');
 
-keyboard
+% keyboard
 
 %% 3) Scroll through successive dropped frames and add them in
 %   - if the # of dropped frames is 1, interpolate, otherwise replace with
@@ -124,25 +124,30 @@ keyboard
 
 n_good_use = 1;
 for k = 1:end_frame
-   if sum(k == real_frame_ind_all) == 1 % Good (non-dropped) frame
-       Fuse = h5read(infile,'/Object',[1 1 n_good_use 1],[XDim YDim 1 1]); % Grab good frame
-       h5write(outfile,'/Object',uint16(Fuse),[1 1 k 1],[XDim YDim 1 1]); % write good frame to the appropriate spot
-       n_good_use = n_good_use + 1; % update current good frame to use
-   elseif sum(k == dropped_frames_all) == 1 % Dropped frame
-       Fuse = h5read(infile,'/Object',[1 1 n_good_use 1],[XDim YDim 1 1]); % Grab good frame
-       h5write(outfile,'/Object',uint16(Fuse),[1 1 k 1],[XDim YDim 1 1]); % write good frame to the appropriate spot
-       
-       % interpolate if it is a single, isolated dropped frame!
-%        if 
-%            
-%        end
-   end
+    try
+        if sum(k == real_frame_ind_all) == 1 % Good (non-dropped) frame
+            Fuse = h5read(infile,'/Object',[1 1 n_good_use 1],[XDim YDim 1 1]); % Grab good frame
+            h5write(outfile,'/Object',uint16(Fuse),[1 1 k 1],[XDim YDim 1 1]); % write good frame to the appropriate spot
+            n_good_use = n_good_use + 1; % update current good frame to use
+        elseif sum(k == dropped_frames_all) == 1 % Dropped frame
+            Fuse = h5read(infile,'/Object',[1 1 n_good_use 1],[XDim YDim 1 1]); % Grab good frame
+            h5write(outfile,'/Object',uint16(Fuse),[1 1 k 1],[XDim YDim 1 1]); % write good frame to the appropriate spot
+            % interpolate if it is a single, isolated dropped frame!
+            %        if
+            %
+            %        end
+        end
+    catch
+        disp('Error in the above - check it!')
+        keyboard
+    end
 end
 
 % Check to make sure everything worked
 if n_good_use ~= sum(frames)
-    disp('Did not use all the original frames.  Something is screwed up!')
+    disp('Did not use all the original frames.  Something might be screwed up!')
 end
 
+%%
 
 end
