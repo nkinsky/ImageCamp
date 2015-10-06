c6bdc447ecaf34cfd65862477a856056376963df
nkinsky <kinsky@bu.edu>
2015-10-06 13:25:27 -0400

    typo fix

--- GCamp/neuron_reg_batch.m
+++ GCamp/neuron_reg_batch.m
@@ -1,7 +1,7 @@
 function [ batch_session_map ] = neuron_reg_batch(base_struct, reg_struct)
 % neuron_reg_batch(base_struct, reg_struct)
 %   Registers the neurons in reg_struct to base_struct and also too each
-%   other.  It runs through this two way: 1) by always registering each
+%   other.  It runs through this two ways: 1) by always registering each
 %   neuron to the base session, or for new neurons found after the first
 %   registration, to the oldest session, and 2) by always updating each
 %   neuron mask to the most recently registered neuron.  After this, it
