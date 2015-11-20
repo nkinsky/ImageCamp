5aa075052760c587f2ed21c0e0efd060e7405c55
nkinsky <kinsky@bu.edu>
2015-11-20 11:20:44 -0500

    Caught some erroneous assignments

--- GCamp/Project specific code/2env/twoenv_betterindices.m
+++ GCamp/Project specific code/2env/twoenv_betterindices.m
@@ -23,10 +23,13 @@ before_6_conflict{1} = [1 1 6; 1 2 6; 1 3 6; 1 4 6; 2 1 6; 2 2 6; 2 3 6; 2 4 6];
 before_6_aligned{1} = [];
 
 after_5_conflict{1} = [1 5 8];
-after_5_aligned{1} = [1 5 7; 1 5 8; 2 5 8];
+after_5_aligned{1} = [1 5 7; 2 5 7; 2 5 8];
 
 after_6_conflict{1} = [ 1 6 7; 1 6 8; 2 6 7; 2 6 8];
-after_6_conflict{1} = [];
+after_6_aligned{1} = [];
+
+conn1_conn2_conflict{1} = [1 5 6; 2 5 6];
+conn1_conn2_aligned{1} = [];
 
 % G31
 before_win_conflict{2} = [1 1 2; 1 1 4; 1 2 3; 1 2 4; 1 3 4; 2 1 2; 2 1 4; 2 2 3; 2 2 4; 2 3 4];
@@ -45,10 +48,13 @@ before_6_conflict{2} = [1 1 6; 1 2 6; 1 3 6; 1 4 6; 2 1 6; 2 2 6; 2 3 6; 2 4 6];
 before_6_aligned{2} = [];
 
 after_5_conflict{2} = [1 5 8; 2 5 8];
-after_5_aligned{2} = [1 5 7; 1 5 8];
+after_5_aligned{2} = [1 5 7; 2 5 7];
 
 after_6_conflict{2} = [ 1 6 7; 1 6 8; 2 6 7; 2 6 8];
-after_6_conflict{2} = [];
+after_6_aligned{2} = [];
+
+conn1_conn2_conflict{2} = [1 5 6; 2 5 6];
+conn1_conn2_aligned{2} = [];
 
 % G45
 before_win_conflict{3} = [1 1 2; 1 1 4; 1 2 3; 1 2 4; 1 3 4; 2 1 2; 2 1 4; 2 2 3; 2 2 4; 2 3 4];
@@ -67,10 +73,13 @@ before_6_conflict{3} = [1 1 6; 1 2 6; 1 3 6; 1 4 6; 2 1 6; 2 2 6; 2 3 6; 2 4 6];
 before_6_aligned{3} = [];
 
 after_5_conflict{3} = [1 5 8; 2 5 8];
-after_5_aligned{3} = [1 5 7; 1 5 8];
+after_5_aligned{3} = [1 5 7; 2 5 7];
 
 after_6_conflict{3} = [ 1 6 7; 1 6 8; 2 6 7; 2 6 8];
-after_6_conflict{3} = [];
+after_6_aligned{3} = [];
+
+conn1_conn2_conflict{3} = [1 5 6; 2 5 6];
+conn1_conn2_aligned{3} = [];
 
 % G48
 
@@ -90,11 +99,40 @@ before_6_conflict{4} = [1 1 6; 1 2 6; 1 3 6; 1 4 6; 2 1 6; 2 2 6; 2 3 6; 2 4 6];
 before_6_aligned{4} = [];
 
 after_5_conflict{4} = [1 5 8; 2 5 8];
-after_5_aligned{4} = [1 5 7; 1 5 8];
+after_5_aligned{4} = [1 5 7; 2 5 7];
 
 after_6_conflict{4} = [ 1 6 7; 1 6 8; 2 6 7; 2 6 8];
-after_6_conflict{4} = [];
+after_6_aligned{4} = [];
 
+conn1_conn2_conflict{4} = [1 5 6; 2 5 6];
+conn1_conn2_aligned{4} = [];
+%% Combine all
+
+conflict_indices_all = [];
+aligned_indices_all = [];
+for j = 1:length(before_win_conflict)
+   conflict_indices_all{j} = [before_win_conflict{j}; ...
+       after_win_conflict{j}; before_after_conflict{j}; before_5_conflict{j}; ...
+       before_6_conflict{j}; after_5_conflict{j}; after_6_conflict{j}; ...
+       conn1_conn2_conflict{j}];
+   aligned_indices_all{j} = [before_win_aligned{j}; ...
+       after_win_aligned{j}; before_after_aligned{j}; before_5_aligned{j}; ...
+       before_6_aligned{j}; after_5_aligned{j}; after_6_aligned{j}; 
+       conn1_conn2_aligned{j}];
+end
+
+%% Time indices
+
+time_index = [1 1 2 0; 1 1 3 3; 1 1 4 3; 1 1 5 4; 1 1 6 5; 1 1 7 6; 1 1 8 6;...
+    1 2 3 3; 1 2 4 3; 1 2 5 4; 1 2 6 5; 1 2 7 6; 1 2 8 6; ...
+    1 3 4 0; 1 3 5 1; 1 3 6 2; 1 3 7 3; 1 3 8 3; ...
+    1 4 5 1; 1 4 6 2; 1 4 7 3; 1 4 8 3; 1 5 6 1; 1 5 7 2; 1 5 8 2; ...
+    1 6 7 1; 1 6 8 1; 1 7 8 0; ...
+    2 1 2 0 ; 2 1 3 1 ; 2 1 4 1 ; 2 1 5 3 ; 2 1 6 4 ; 2 1 7 6 ; 2 1 8 6 ; ...
+    2 2 3 1 ; 2 2 4 1 ; 2 2 5 3 ; 2 2 6 4 ; 2 2 7 6 ; 2 2 8 6 ; ...
+    2 3 4 0 ; 2 3 5 2 ; 2 3 6 3 ; 2 3 7 5 ; 2 3 8 5 ; ...
+    2 4 5 2 ; 2 4 6 3 ; 2 4 7 5 ; 2 4 8 5 ; 2 5 6 1 ; 2 5 7 3 ; 2 5 8 3 ; ...
+    2 6 7 2 ; 2 6 8 2 ; 2 7 8 0];
 
 
 
