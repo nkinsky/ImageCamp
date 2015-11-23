6d2fe654f6e671aaa3ece9be3224f2d63fb0d8ed
nkinsky <kinsky@bu.edu>
2015-11-23 11:27:23 -0500

    Attempts to simplify using sub-functions

--- GCamp/Project specific code/2env/plot_simplified_summary.m
+++ GCamp/Project specific code/2env/plot_simplified_summary.m
@@ -8,7 +8,8 @@ function [ ] = plot_simplified_summary(local_stat, distal_stat, varargin )
 %%
 error_on = 1; % default
 plot_shuffle = 0; % default
-both_stat = []; % default
+both_stat = local_stat; % Faked variable here, not used in actual plots
+plot_both_stat = 0; % default
 for j = 1:length(varargin)
    if strcmpi(varargin{j},'error_on')
        error_on = varargin{j+1};
@@ -16,44 +17,62 @@ for j = 1:length(varargin)
        plot_shuffle = varargin{j+1};   
    elseif strcmpi(varargin{j},'both_stat')
        both_stat = varargin{j+1};
+       plot_both_stat = 1;
    end
 end
 
 %% Parse out variables for below
-separate_win_local_mean = local_stat.separate_win.mean;
-separate_win_distal_mean = distal_stat.separate_win.mean;
-separate_win_both_mean = both_stat.separate_win.mean;
-separate_win_local_sem = local_stat.separate_win.sem;
-separate_win_distal_sem = distal_stat.separate_win.sem;
-separate_win_both_sem = both_stat.separate_win.sem;
 
-sep_conn1_local_mean = local_stat.sep_conn1.mean;
-sep_conn1_distal_mean = distal_stat.sep_conn1.mean;
-sep_conn1_both_mean = both_stat.sep_conn1.mean;
-sep_conn1_local_sem = local_stat.sep_conn1.sem;
-sep_conn1_distal_sem = distal_stat.sep_conn1.sem;
-sep_conn1_both_sem = both_stat.sep_conn1.sem;
+[separate_win_local_mean, separate_win_local_sem] = parse_stat_vars(local_stat.separate_win);
+[separate_win_distal_mean, separate_win_distal_sem] = parse_stat_vars(distal_stat.separate_win);
+[separate_win_both_mean, separate_win_both_sem] = parse_stat_vars(both_stat.separate_win);
 
-sep_conn2_local_mean = local_stat.sep_conn2.mean;
-sep_conn2_distal_mean = distal_stat.sep_conn2.mean;
-sep_conn2_both_mean = both_stat.sep_conn2.mean;
-sep_conn2_local_sem = local_stat.sep_conn2.sem;
-sep_conn2_distal_sem = distal_stat.sep_conn2.sem;
-sep_conn2_both_sem = both_stat.sep_conn2.sem;
+[sep_conn1_local_mean, sep_conn1_local_sem] = parse_stat_vars(local_stat.sep_conn1);
+[sep_conn1_distal_mean, sep_conn1_distal_sem] = parse_stat_vars(distal_stat.sep_conn1);
+[sep_conn1_both_mean, sep_conn1_both_sem] = parse_stat_vars(both_stat.sep_conn1);
 
-before_after_local_mean = local_stat.before_after.mean;
-before_after_distal_mean = distal_stat.before_after.mean;
-before_after_both_mean = both_stat.before_after.mean;
-before_after_local_sem = local_stat.before_after.sem;
-before_after_distal_sem = distal_stat.before_after.sem;
-before_after_both_sem = both_stat.before_after.sem;
+[sep_conn2_local_mean, sep_conn2_local_sem] = parse_stat_vars(local_stat.sep_conn2);
+[sep_conn2_distal_mean, sep_conn2_distal_sem] = parse_stat_vars(distal_stat.sep_conn2);
+[sep_conn2_both_mean, sep_conn2_both_sem] = parse_stat_vars(both_stat.sep_conn2);
+
+[before_after_local_mean, before_after_local_sem] = parse_stat_vars(local_stat.before_after);
+[before_after_distal_mean, before_after_distal_sem] = parse_stat_vars(distal_stat.before_after);
+[before_after_both_mean, before_after_both_sem] = parse_stat_vars(both_stat.before_after);
+
+% separate_win_local_mean = local_stat.separate_win.mean;
+% separate_win_distal_mean = distal_stat.separate_win.mean;
+% separate_win_both_mean = both_stat.separate_win.mean;
+% separate_win_local_sem = local_stat.separate_win.sem;
+% separate_win_distal_sem = distal_stat.separate_win.sem;
+% separate_win_both_sem = both_stat.separate_win.sem;
+
+% sep_conn1_local_mean = local_stat.sep_conn1.mean;
+% sep_conn1_distal_mean = distal_stat.sep_conn1.mean;
+% sep_conn1_both_mean = both_stat.sep_conn1.mean;
+% sep_conn1_local_sem = local_stat.sep_conn1.sem;
+% sep_conn1_distal_sem = distal_stat.sep_conn1.sem;
+% sep_conn1_both_sem = both_stat.sep_conn1.sem;
+% 
+% sep_conn2_local_mean = local_stat.sep_conn2.mean;
+% sep_conn2_distal_mean = distal_stat.sep_conn2.mean;
+% sep_conn2_both_mean = both_stat.sep_conn2.mean;
+% sep_conn2_local_sem = local_stat.sep_conn2.sem;
+% sep_conn2_distal_sem = distal_stat.sep_conn2.sem;
+% sep_conn2_both_sem = both_stat.sep_conn2.sem;
+% 
+% before_after_local_mean = local_stat.before_after.mean;
+% before_after_distal_mean = distal_stat.before_after.mean;
+% before_after_both_mean = both_stat.before_after.mean;
+% before_after_local_sem = local_stat.before_after.sem;
+% before_after_distal_sem = distal_stat.before_after.sem;
+% before_after_both_sem = both_stat.before_after.sem;
 
 %% Plot stuff
-if isempty(both_stat)
+if plot_both_stat == 0
     h = bar([separate_win_local_mean, separate_win_distal_mean;  ...
         sep_conn1_local_mean, sep_conn1_distal_mean; sep_conn2_local_mean, sep_conn2_distal_mean;...
         before_after_local_mean, before_after_distal_mean]);
-else
+elseif plot_both_stat == 1
     h = bar([separate_win_local_mean, separate_win_distal_mean, separate_win_both_mean;  ...
         sep_conn1_local_mean, sep_conn1_distal_mean sep_conn1_both_mean; ...
         sep_conn2_local_mean, sep_conn2_distal_mean, sep_conn2_both_mean;...
@@ -67,7 +86,7 @@ if error_on == 1
     errorbar(h(2).XData + h(2).XOffset, [separate_win_distal_mean, ...
         sep_conn1_distal_mean, sep_conn2_distal_mean, before_after_distal_mean], [separate_win_distal_sem, ...
         sep_conn1_distal_sem, sep_conn2_distal_sem, before_after_distal_sem],'.')
-    if ~isempty(both_stat)
+    if plot_both_stat == 1
        errorbar(h(3).XData + h(3).XOffset, [separate_win_both_mean, ...
         sep_conn1_both_mean, sep_conn2_both_mean, before_after_both_mean], [separate_win_both_sem, ...
         sep_conn1_both_sem, sep_conn2_both_sem, before_after_both_sem],'.') 
@@ -76,15 +95,15 @@ end
 
 if plot_shuffle == 1
         h2 = plot(get(gca,'XLim'),[shuffle_mean shuffle_mean],'r--');
-    if ~isempty(both_stat)
+    if plot_both_stat == 0
         h_legend = legend([h(1) h(2) h2],'Local cues aligned','Distal cues aligned','Chance (Shuffled Data)');
-    else
+    elseif plot_both_stat == 1
         h_legend = legend([h(1) h(2) h(3) h2],'Local cues aligned','Distal cues aligned','Both aligned','Chance (Shuffled Data)');
     end
 else
-    if ~isempty(both_stat)
+    if plot_both_stat == 0
         h_legend = legend([h(1) h(2)],'Local cues aligned','Distal cues aligned');
-    else
+    elseif plot_both_stat == 1
         h_legend = legend([h(1) h(2) h(3)],'Local cues aligned','Distal cues aligned', 'Both aligned');
     end
 end
@@ -95,4 +114,19 @@ hold off
 ylims_given = get(gca,'YLim');
 
 end
+
+%% sub-function
+function [stat_out_mean, stat_out_sem] = parse_stat_vars(stat_var_in)
+%%
+if ~isempty(stat_var_in)
+    stat_out_mean = stat_var_in.mean;
+    stat_out_sem = stat_var_in.sem;
+else
+    stat_out_mean = nan;
+    stat_out_sem = nan;
+end
+%%
+
+end
+
 
--- GCamp/Project specific code/2env/twoenv_get_ind_mean.m
+++ GCamp/Project specific code/2env/twoenv_get_ind_mean.m
@@ -9,7 +9,8 @@ function [ local_stat, distal_stat, both_stat ] = twoenv_get_ind_mean(Mouse_stru
 %   [1 5 8] compares the 5th session to the 8th session).  If there are 3
 %   columns then the 1st column designates which arena to look at (1 =
 %   square, 2 = octagon).  If there are only two columns then it will look
-%   at the same sessions in both the square and the circle
+%   at the same sessions in both the square and the circle.  Number of
+%   columns must be consistent across all
 
 %% Initialize variables and get varargins
 temp_local = [];
@@ -21,41 +22,100 @@ for j = 1:length(varargin)
    end
 end
 
-%% Check for
+%% Check if arena type is specified in the vectors - if not, then add it in
 
-for k = 1:length(Mouse_struct)
+local_sub_use = check_indices_type(local_sub_use);
+distal_sub_use = check_indices_type(distal_sub_use);
+both_sub_use = check_indices_type(both_sub_use);
+
+%% Create _stat variables
+
+distal_stat = calc_group_stats(Mouse_struct, distal_sub_use,1);
+local_stat = calc_group_stats(Mouse_struct, local_sub_use, 2);
+
+if ~isempty(both_sub_use)
+    both_stat = calc_group_stats(Mouse_struct, both_sub_use,1);
+else
+    both_stat = [];
+end
+
+% for k = 1:length(Mouse_struct)
+%     for j = 1:2
+%             local_indices_use = make_mega_sub2ind(size(Mouse_struct(k).corr_matrix{loca_indices,j}),...
+%                 local_sub_use(:,1),local_sub_use(:,2));
+%             distal_indices_use = make_mega_sub2ind(size(Mouse_struct(k).corr_matrix{1,j}),...
+%                 distal_sub_use(:,1),distal_sub_use(:,2));
+%             
+%             temp_distal = [temp_distal; Mouse_struct.corr_matrix{1,j}(distal_indices_use)];
+%             temp_local = [temp_local; Mouse_struct.corr_matrix{2,j}(local_indices_use)];
+%             
+%             if ~isempty(both_sub_use)
+%                both_indices_use = make_mega_sub2ind(size(Mouse_struct(k).corr_matrix{1,j}),...
+%                    both_sub_use(:,1),both_sub_use(:,2));
+%                temp_both = [temp_both; Mouse_struct.corr_matrix{2,j}(both_indices_use)];
+%             end
+%     end
+% end
+% 
+% local_stat.all = temp_local;
+% local_stat.mean = nanmean(temp_local);
+% local_stat.sem = nanstd(temp_local)/sqrt(length(local_indices_use));
+% 
+% distal_stat.all = temp_distal;
+% distal_stat.mean = nanmean(temp_distal);
+% distal_stat.sem = nanstd(temp_distal)/sqrt(length(distal_indices_use));
+% 
+% if isempty(both_sub_use)
+%     both_stat = [];
+% else
+%     both_stat.all = temp_both;
+%     both_stat.mean = nanmean(temp_both);
+%     both_stat.sem = nanstd(temp_both)/sqrt(length(both_indices_use));
+% end
+    
+end
+
+%% sub-function to check indices
+function [indices_fix] = check_indices_type(indices_in)
+%%
+num_comparisons = size(indices_in,1);
+if size(indices_in,2) == 2
+    indices_fix = zeros(2*num_comparisons,3);
+    indices_fix(1:num_comparisons,1) = ones(num_comparisons,1);
+    indices_fix(num_comparisons + 1:end,1) = 2*ones(num_comparisons,1);
+    indices_fix(:,2:3) = repmat(indices_in,2,1);
+else
+    indices_fix = indices_in;
+end
+%%
+end
+
+%% sub-fuction to get correlations out and calculate mean/sem
+function [stat_use] = calc_group_stats(animal_struct, sub_use, local_flag)
+% local_flag: 1 = use distal aligned comparisons, 2 = use local aligned
+% comparisons
+all_out = [];
+all_means = [];
+temp = [];
+num_comparisons = size(sub_use,1);
+for k = 1:length(animal_struct)
     for j = 1:2
-            local_indices_use = make_mega_sub2ind(size(Mouse_struct(k).corr_matrix{1,j}),...
-                local_sub_use(:,1),local_sub_use(:,2));
-            distal_indices_use = make_mega_sub2ind(size(Mouse_struct(k).corr_matrix{1,j}),...
-                distal_sub_use(:,1),distal_sub_use(:,2));
+        for arena_use = 1:2
+            sub_use_index = sub_use(arena_use == sub_use(:,1)); % Pull-out subs for appropriate arena
+            indices_use = make_mega_sub2ind(size(animal_struct(k).corr_matrix{arena_use,j}),...
+                sub_use(sub_use_index,2),sub_use(sub_use_index,3)); % convert subs to indices
             
-            temp_distal = [temp_distal; Mouse_struct.corr_matrix{1,j}(distal_indices_use)];
-            temp_local = [temp_local; Mouse_struct.corr_matrix{2,j}(local_indices_use)];
-            
-            if ~isempty(both_sub_use)
-               both_indices_use = make_mega_sub2ind(size(Mouse_struct(k).corr_matrix{1,j}),...
-                   both_sub_use(:,1),both_sub_use(:,2));
-               temp_both = [temp_both; Mouse_struct.corr_matrix{2,j}(both_indices_use)];
-            end
+            temp = animal_struct.corr_matrix{local_flag,j}(indices_use);
+            all_out = [all_out; temp]; % Add appropriate comparisons into temp_out
+            all_means = [all_means; nanmean(temp)];
+        end
     end
 end
 
-local_stat.all = temp_local;
-local_stat.mean = nanmean(temp_local);
-local_stat.sem = nanstd(temp_local)/sqrt(length(local_indices_use));
+stat_use.all = all_out;
+stat_use.all_means = all_means;
+stat_use.mean = nanmean(all_out);
+stat_use.sem = nanstd(all_out)/sqrt(num_comparisons);
 
-distal_stat.all = temp_distal;
-distal_stat.mean = nanmean(temp_distal);
-distal_stat.sem = nanstd(temp_distal)/sqrt(length(distal_indices_use));
-
-if isempty(both_sub_use)
-    both_stat = [];
-else
-    both_stat.all = temp_both;
-    both_stat.mean = nanmean(temp_both);
-    both_stat.sem = nanstd(temp_both)/sqrt(length(both_indices_use));
-end
-    
 end
 
