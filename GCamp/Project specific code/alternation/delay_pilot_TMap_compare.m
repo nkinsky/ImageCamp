485258e4f8edea19325d04f50a91ccdc2e3edebb
nkinsky <kinsky@bu.edu>
2016-01-26 16:56:07 -0500

    More general filter

--- GCamp/Project specific code/alternation/delay_pilot_TMap_compare.m
+++ GCamp/Project specific code/alternation/delay_pilot_TMap_compare.m
@@ -23,40 +23,43 @@ pval_delay = pval;
 neuron_use_either = find(pval_continuous > (1 - pval_thresh) | ...
     pval_delay > (1-pval_thresh));
 
-%scroll through and plot everything
+%% scroll through and plot everything
+
+neurons_to_plot = stable; 
+
 figure(501); 
 cm = colormap('jet');
-for j = 1:length(neuron_use_either) 
+for j = 1:length(neurons_to_plot) 
     
     % Make non-occupied spots white
     [~, TMap_cont_nan] = make_nan_TMap(RunOccMap_continuous,...
-        TMap_continuous{neuron_use_either(j)}); % Continuous blocks
+        TMap_continuous{neurons_to_plot(j)}); % Continuous blocks
     [~, TMap_delay_nan] = make_nan_TMap(RunOccMap_delay,...
-        TMap_delay{neuron_use_either(j)}); % Delayed blocks
+        TMap_delay{neurons_to_plot(j)}); % Delayed blocks
     
-    corr_plot(j) = corr(TMap_continuous{neuron_use_either(j)}(:),...
-        TMap_delay{neuron_use_either(j)}(:)); % Get correlation value
+    corr_plot(j) = corr(TMap_continuous{neurons_to_plot(j)}(:),...
+        TMap_delay{neurons_to_plot(j)}(:)); % Get correlation value
     
 %     % Make non-occupied spots white - continuous by block 
 %     for k = 1:2
 %         [~, TMap_cont_half_nan{k}] = make_nan_TMap(RunOccMap,...
-%             TMap_cont_half(k).TMap_gauss{neuron_use_either(j)}); 
+%             TMap_cont_half(k).TMap_gauss{neurons_to_plot(j)}); 
 %     end
     
     subplot(2,1,1); 
     imagesc_nan(rot90(TMap_cont_nan,1), cm, [1 1 1]);
-    title(['Continuous - neuron ' num2str(neuron_use_either(j)) ...
+    title(['Continuous - neuron ' num2str(neurons_to_plot(j)) ...
         ' with correlation = ' num2str(corr_plot(j))])
     
     subplot(2,1,2); 
     imagesc_nan(rot90(TMap_delay_nan,1), cm, [1 1 1]);
-    title(['Delayed - neuron ' num2str(neuron_use_either(j))])
+    title(['Delayed - neuron ' num2str(neurons_to_plot(j))])
     
 %     half_name = {'1st','2nd'};
 %     for k = 1:2
 %         subplot(2,2,2+k)
 %         imagesc_nan(TMap_cont_half_nan{k},dm,[1 1 1]);
-%         title(['Continuous ' half_name{k} ' - neuron ' num2str(neuron_use_either(j))])
+%         title(['Continuous ' half_name{k} ' - neuron ' num2str(neurons_to_plot(j))])
 %     end
 %     
     waitforbuttonpress; 
