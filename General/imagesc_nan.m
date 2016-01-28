a7ed6034b47e741ff3c6bc05e5f6ffb1448fa225
nkinsky <kinsky@bu.edu>
2016-01-28 11:23:34 -0500

    Enhancement = can set CLim to arbitrary values now

--- General/imagesc_nan.m
+++ General/imagesc_nan.m
@@ -1,5 +1,14 @@
-function [h, hcb] = imagesc_nan(a,cm,nanclr)
+function [h, hcb] = imagesc_nan(a,cm,nanclr,CLim)
 % function [h, hcb] = imagesc_nan(a,cm,nanclr)
+%  Plots just like imagesc, but with any nans labeled as nanclr
+%
+% a = matrix to plot with imagesc
+% cm = colormap to use (e.g. cm = colormap('jet');
+% nanclr = rgb color triplet, e.g. nanclr = [1 1 1] sets all NaN values to
+% white
+% CLim(optional) - if you want to adjust the scale of CLim artificially, enter
+% 'CLim',[cmin cmax]. If left blank the max and min values will be set by
+% the max and min of a.
 
 %% Old try
 % imagesc_nan(im_plot, varargin)
@@ -12,8 +21,13 @@ function [h, hcb] = imagesc_nan(a,cm,nanclr)
 
 %% New try (found online at http://stackoverflow.com/questions/8481324/contrasting-color-for-nans-in-imagesc)
 %# find minimum and maximum
-amin=min(a(:));
-amax=max(a(:));
+if nargin < 4
+    amin=min(a(:));
+    amax=max(a(:));
+elseif nargin == 4
+    amin = CLim(1);
+    amax = CLim(2);
+end
 %# size of colormap
 n = size(cm,1);
 %# color step
