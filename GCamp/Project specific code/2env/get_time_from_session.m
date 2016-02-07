995867daf00305209b1037428257016cc6afbc04
nkinsky <kinsky@bu.edu>
2016-02-07 17:32:05 -0500

    bugfix

--- GCamp/Project specific code/2env/get_time_from_session.m
+++ GCamp/Project specific code/2env/get_time_from_session.m
@@ -1,15 +1,19 @@
 function [ time_out ] = get_time_from_session( index_use, time_index )
 % time_out  = get_time_from_session( index_use, time_index )
-%  Takes indices in index_use (column 1 = arena, column 2 = 1st session, columb 3 =
+%  Takes indices in index_use (column 1 = arena, column 2 = 1st session, column 3 =
 %  2nd session) and compares it to the time_index array (same as index_use
 %  but with column 4 = time in days between sessions)
 
 time_logical = [];
 time_out = nan(size(index_use,1),1);
 for j = 1: size(index_use,1)
-   time_logical = index_use(j,1) == time_index(:,1) & ...
-       index_use(j,2) == time_index(:,2) & index_use(j,3) == time_index(:,3);
-   time_out(j) = time_index(time_logical,4);
+    if index_use(j,2) == index_use(j,3) % Assign time = 0 between the same sessions
+        time_out(j) = 0;
+    else
+        time_logical = index_use(j,1) == time_index(:,1) & ...
+            index_use(j,2) == time_index(:,2) & index_use(j,3) == time_index(:,3);
+        time_out(j) = time_index(time_logical,4);
+    end
 end
 
 end
