3972fbd96e8112113412ccbf00ca1b53e89ab71b
nkinsky <kinsky@bu.edu>
2016-02-29 09:19:47 -0500

    Added dirstr as output.

--- GCamp/helpers/ChangeDirectory_NK.m
+++ GCamp/helpers/ChangeDirectory_NK.m
@@ -1,9 +1,11 @@
-function [ ] = ChangeDirectory_NK(MD_in )
-%Lazy man's ChangeDirectory - takes only the appropriate session from MD as
-%input
+function [ dirstr ] = ChangeDirectory_NK(MD_in)
+% dirstr = ChangeDirectory_NK(MD_in)
+%
+% Lazy man's ChangeDirectory - takes only the appropriate session from MD as
+% input
 
 
-ChangeDirectory(MD_in.Animal,MD_in.Date,MD_in.Session);
+dirstr = ChangeDirectory(MD_in.Animal,MD_in.Date,MD_in.Session);
 
 end
 
