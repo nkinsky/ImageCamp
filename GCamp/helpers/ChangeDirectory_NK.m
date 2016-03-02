b7a01f648a966b1349a24cbca92d95a595b56b1c
nkinsky <kinsky@bu.edu>
2016-03-02 14:44:21 -0500

    Made changing directory optional.

--- GCamp/helpers/ChangeDirectory_NK.m
+++ GCamp/helpers/ChangeDirectory_NK.m
@@ -1,11 +1,14 @@
-function [ dirstr ] = ChangeDirectory_NK(MD_in)
+function [ dirstr ] = ChangeDirectory_NK(MD_in,change_dir_flag)
 % dirstr = ChangeDirectory_NK(MD_in)
 %
 % Lazy man's ChangeDirectory - takes only the appropriate session from MD as
 % input
 
+if ~exist('change_dir_flag','var')
+    change_dir_flag = 1; % default value
+end
 
-dirstr = ChangeDirectory(MD_in.Animal,MD_in.Date,MD_in.Session);
+dirstr = ChangeDirectory(MD_in.Animal,MD_in.Date,MD_in.Session,change_dir_flag);
 
 end
 
