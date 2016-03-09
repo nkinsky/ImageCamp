c08d4644acda4132d8fc86e171b10ff30d68a035
nkinsky <kinsky@bu.edu>
2016-03-09 14:55:56 -0500

    Initial commit

---
+++ GCamp/Project specific code/2env/qc_COM_dist.m
@@ -0,0 +1,58 @@
+sesh_compare = [1 2];
+Mouse_comp = 1;
+arena_use = 1; % 1 = square, 2 = circle
+align_type = 1; % 1 = distal, 2 = local
+PF_thresh = 0.9; % input value for get_PF_centroid
+
+% Get batch_session_map to use
+batch_map_use = fix_batch_session_map(Mouse(Mouse_comp).batch_session_map(arena_use));
+vec_use = Mouse(Mouse_comp).vec_diff_matrix{arena_use,align_type}(sesh_compare(1),...
+    sesh_compare(2));
+PFfilename = {'PlaceMaps.mat','PlaceMaps_rot_to_std.mat'};
+
+% Load TMaps
+PF_centroid_plot = cell(2,1);
+map_plot(2).TMap = [];
+for j = 1:2
+    load(fullfile(ChangeDirectory_NK(batch_map_use.session(sesh_compare(j))),...
+        PFfilename{align_type}),'TMap_gauss','RunOccMap');
+    [~, map_plot(j).TMap] = cellfun(@(a) make_nan_TMap(RunOccMap,a), ...
+        TMap_gauss,'UniformOutput',0);
+    PF_centroid_plot{j} = get_PF_centroid(TMap_gauss,PF_thresh);
+end
+
+%%
+figure(124)
+cm = colormap('jet');
+for j = 1:size(batch_map_use.map,1)
+    sesh_neuron(1) = batch_map_use.map(j,1 + sesh_compare(1)); % Get neuron number for each session
+    sesh_neuron(2) = batch_map_use.map(j,1 + sesh_compare(1));
+    if sesh_neuron(1) == 0 && sesh_neuron(2) == 0
+        continue
+    else
+        for k = 1:2
+            if sesh_neuron(k) ~= 0
+                subplot(2,2,k)
+                imagesc_nan(map_plot(k).TMap{sesh_neuron(k)},cm,[1 1 1]);
+                set(gca,'YDir','Normal')
+                hold on
+                num_fields = sum(~cellfun(@isempty,PF_centroid_plot{k}(sesh_neuron(k),:))); % Get number of valid fields
+                for ll = 1:num_fields % Plot centroids
+                plot(PF_centroid_plot{k}{sesh_neuron(k),ll}(1),...
+                    PF_centroid_plot{k}{sesh_neuron(k),ll}(2),'w*')
+                end
+                quiver(vec_use.xy(sesh_neuron(1),1),vec_use.xy(sesh_neuron(1),2),...
+                    vec_use.uv(sesh_neuron(1),1),vec_use.uv(sesh_neuron(1),2),0,'r');
+                title(['xy = ' num2str(vec_use.xy(sesh_neuron(1),:))])
+                hold off
+            end
+        end
+        subplot(2,2,3)
+        h = quiver(vec_use.xy(:,1),vec_use.xy(:,2),vec_use.uv(:,1),vec_use.uv(:,2),0,'b');
+        
+    end
+    
+    
+    waitforbuttonpress
+end
+
---
+++ GCamp/Project specific code/2env/twoenv_get_hallway_PFincrease.m
@@ -0,0 +1,64 @@
+function [ conn_hw_mean, conn_hw_sem, conn_nonhw_mean, conn_nonhw_sem ] = twoenv_get_hallway_PFincrease( grid_sum_comb, hw_grid_row, hw_grid_col, arena_type, before_after )
+% [ conn_hw_mean, conn_hw_sem, conn_nonhw_mean, conn_nonhw_sem ] = twoenv_get_hallway_PFincrease( grid_sum_comb, hw_grid_row, hw_grid_col )
+%   Breaks apart increases in place field density near the hallway versus
+%   the rest of the grids.  
+%   INPUTS:
+%
+%   grid_sum_comb, a num_arenas x num_sessions x num_sessions x num_grids 
+%   x num_grids array with the mean PFincrease values for all mice.
+%
+%   hw_grid_row, hw_grid_col: location of the grid near the hallway
+%
+%   arena_type: 1 = square, 2 = circle
+%
+%   before_after (optional): 0(default) = get connected sessions -
+%   unconnected sessions, 1 = get session 7 minus session 4, 2 = get
+%   sessions 7-8 minus sessions 1-4
+
+if nargin < 5
+    before_after = 0;
+end
+
+if before_after == 0
+    temp1 = grid_sum_comb(sub2ind(size(grid_sum_comb),arena_type*ones(8,1),[1:4 1:4]',...
+        [5 5 5 5 6 6 6 6]', hw_grid_row*ones(8,1), hw_grid_col*ones(8,1))); % all values for grid by hallway in session 5 and 6 versus sessions 1-4
+    temp2 = grid_sum_comb(sub2ind(size(grid_sum_comb),arena_type*ones(4,1),[5:6 5:6]',...
+        [7 7 8 8]', hw_grid_row*ones(4,1), hw_grid_col*ones(4,1)));
+    conn_hallwaygrid_PFdensincrease = [temp1; -temp2];
+elseif before_after == 1
+     temp1 = squeeze(grid_sum_comb(arena_type,4,7,hw_grid_row,hw_grid_col));
+     conn_hallwaygrid_PFdensincrease = temp1(:);
+elseif before_after == 2
+    conn_hallwaygrid_PFdensincrease = grid_sum_comb(sub2ind(size(grid_sum_comb),arena_type*ones(8,1),[1:4 1:4]',...
+        [7 7 7 7 8 8 8 8]', hw_grid_row*ones(8,1), hw_grid_col*ones(8,1)));
+end
+
+conn_hw_mean = mean(conn_hallwaygrid_PFdensincrease);
+conn_hw_sem = std(conn_hallwaygrid_PFdensincrease)/...
+    sqrt(length(conn_hallwaygrid_PFdensincrease));
+
+if before_after == 0
+    temp3 = squeeze(grid_sum_comb(arena_type,1:4,5:6,:,:)); % All values for sessions 5 and 6 versus sessions 1-4
+    temp4 = squeeze(grid_sum_comb(arena_type,5:6,7:8,:,:)); % All values for sessions 5 and 6 versus sessions 7-8
+    conn_allgrid_PFdensincrease = [temp3(:); -temp4(:)];
+elseif before_after == 1
+    temp3 = squeeze(grid_sum_comb(arena_type,4,7,:,:));
+    conn_allgrid_PFdensincrease = temp3(:);
+elseif before_after == 2
+    temp3 = squeeze(grid_sum_comb(arena_type,1:4,7:8,:,:));
+    conn_allgrid_PFdensincrease = temp3(:);
+end
+% Now, exclude all the values in the grid near the hallway from the ALL
+% values
+exclude_logical = false(size(conn_allgrid_PFdensincrease));
+for j = 1:length(conn_hallwaygrid_PFdensincrease)
+    exclude_logical = exclude_logical | (conn_hallwaygrid_PFdensincrease(j) == ...
+        conn_allgrid_PFdensincrease);
+end
+conn_allgrid_PFdensincrease = conn_allgrid_PFdensincrease(~exclude_logical);
+conn_nonhw_mean = mean(conn_allgrid_PFdensincrease);
+conn_nonhw_sem = std(conn_allgrid_PFdensincrease)/...
+    sqrt(length(conn_allgrid_PFdensincrease));
+
+end
+
