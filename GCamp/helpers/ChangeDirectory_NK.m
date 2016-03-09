7ac0eafa28951e566e35f4f187001b99b0e2ad82
nkinsky <kinsky@bu.edu>
2016-03-09 14:53:44 -0500

    commenting

--- GCamp/helpers/ChangeDirectory_NK.m
+++ GCamp/helpers/ChangeDirectory_NK.m
@@ -2,7 +2,10 @@ function [ dirstr ] = ChangeDirectory_NK(MD_in,change_dir_flag)
 % dirstr = ChangeDirectory_NK(MD_in)
 %
 % Lazy man's ChangeDirectory - takes only the appropriate session from MD as
-% input
+% input.  
+%
+% change_dir_flag: 1 (default if left blank) - change to this directory in
+% MATLAB workspace, 0 - just spit out dirstr withoug changing directory
 
 if ~exist('change_dir_flag','var')
     change_dir_flag = 1; % default value
