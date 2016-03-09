d0a923ab47f6d80b09fd1c988e51f415d79f1352
nkinsky <kinsky@bu.edu>
2016-03-09 14:55:45 -0500

    added some error catching

--- GCamp/make_binary_TMap.m
+++ GCamp/make_binary_TMap.m
@@ -1,18 +1,26 @@
 function [ TMap_bin ] = make_binary_TMap( TMap, thresh )
-% [ TMap_bin ] = make_binary_TMap( TMap )
+% [ TMap_bin ] = make_binary_TMap( TMap, thresh )
 % Takes a heatmap (TMap) and turns it into a binary place-field map with
-% ones in the placefield and zeros elsewhere.  thresh is the value of the
-% TMap above which you wish to make ones.  Everything below thresh will go
-% to zeros.
+% ones in the placefield and zeros elsewhere.  thresh is the cutoff from the ecdf that you
+% wish to use to set values to 1 (e.g. thresh = 0.9 will set pixels
+% where the smoothed firing rate is greater than 90% of the values in the
+% TMap to 1).  Everything below thresh will go
+% to zeros.  If thresh is NOT specified, it will be auto-selected as 0.9.
 
-% sets threshold to 0 if not specified
+% automatically sets threshold if not specified
 if nargin < 2
-    thresh = 0;
+    thresh = 0.9;
 end
 
 TMap_bin = zeros(size(TMap)); % Set it up
-TMap_bin(TMap > thresh & ~isnan(TMap)) = ...
-    ones(size(find(TMap > thresh & ~isnan(TMap)))); % Make all valid areas = 1
-TMap_bin = logical(TMap_bin);
+try
+    [f, x] = ecdf(TMap(:));
+    thresh_binary = x(find(f > thresh,1,'first'));
+    TMap_bin(TMap > thresh_binary & ~isnan(TMap)) = ...
+        ones(size(find(TMap > thresh_binary & ~isnan(TMap)))); % Make all valid areas = 1
+    TMap_bin = logical(TMap_bin);
+catch % If there is an error of some sort, just leave TMap_bin as zeros.
+end
+    
 end
 
