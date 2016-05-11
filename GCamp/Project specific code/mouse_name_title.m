60767ffcbd431d8c4465acd90bea64604c97e718
nkinsky <kinsky@bu.edu>
2016-05-11 09:54:03 -0400

    bugfix

--- GCamp/Project specific code/mouse_name_title.m
+++ GCamp/Project specific code/mouse_name_title.m
@@ -5,15 +5,19 @@ function [ name_plot ] = mouse_name_title( Animal_name )
 
 % Find where the underscore is
 u_ind = regexp(Animal_name,'_');
-
-name_plot = [Animal_name(1:u_ind-1) '\' Animal_name(u_ind:end)];
-
+% 
+% name_plot = [Animal_name(1:u_ind-1) '\' Animal_name(u_ind:end)];
+% 
 
 start_ind = [1 u_ind];
 end_ind = [u_ind length(Animal_name)];
 name_plot = [];
-for j = 1:length(start_ind)-1
-    name_plot = 
+for j = 1:length(start_ind)
+    if j < length(start_ind)
+        name_plot = [name_plot, Animal_name(start_ind(j):end_ind(j)-1) '\'];
+    else
+        name_plot = [name_plot, Animal_name(start_ind(j):end_ind(j))];
+    end
 end
 
 end
