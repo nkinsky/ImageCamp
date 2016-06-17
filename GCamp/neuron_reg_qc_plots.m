3b1bd65c55980d4d698b86fcc0b7165b37e3ae86
nkinsky <kinsky@bu.edu>
2016-06-17 14:46:36 -0400

    Tons of updates

--- GCamp/neuron_reg_qc_plots.m
+++ GCamp/neuron_reg_qc_plots.m
@@ -30,20 +30,20 @@ close all
 
 %% Step 0 overall: Set base sessions and variables
 
-base_sesh_index = ref.G31.two_env(1) + 2; % Specify the base 2env session to use
-base_sesh = MD(base_sesh_index);
+base_sesh_index = 3; % Specify the base 2env session to use from below
 % Session references:
-% ref.G30.two_env(1) - good
-% ref.G45.twoenv(1)+2 - good, other session (square) remaps
-% ref.G31.two_env(1) + 2 - good, the square remaps between sessions
-% ref.G48.twoenv(1) + 6;
+session_ref(1) = ref.G30.two_env(1); % - good
+session_ref(2) = ref.G45.twoenv(1)+2; % - good, other session (square) remaps
+session_ref(3) = ref.G31.two_env(1) + 2; % - good, the square remaps between sessions
+session_ref(4) = ref.G48.twoenv(1) + 6;
+base_sesh = MD(session_ref(base_sesh_index));
 
 PF_filterspec = 4; % 3 = use only neurons with a valid PF in BOTH session, 4 = either session
 PF_pval_thresh = 0.05; % pval threshold on spatial information for filtering out neurons
-min_thresh = 10; % neurons must be at least this close in pixels to be validly mapped, default = 3
+min_thresh = 2; % neurons must be at least this close in pixels to be validly mapped, default = 3
 multi_map_method = 2; % method for disambiguating multiple mapping neurons (0 = use most overlap, 1 = use closest, 2 = use smallest shape ratio difference)
 num_shuffles = 100;
-reg_self = 0; % 0 = register to next session indicated below, 1 = register session to itself
+reg_self = 1; % 0 = register to next session indicated below, 1 = register session to itself
 fine_res = 0; % 0, Sets shift difference from 0:10 pixels, 1 sets it from 2:3 pixels
 
 % For Part 2 - set file to do comparisons on.  Might want to make this
@@ -353,12 +353,14 @@ legend(h2,h2legend)
 h3legend = h2legend;
 h3legend{length(h2legend) + 1} = 'Shuffled';
 subplot(2,2,3)
+set(h3(end),'LineStyle','--')
 % h = get(gca,'Children'); % Get line handles
 xlabel('Axis ratio difference')
 legend(h3,h3legend)
 
 subplot(2,2,4)
 % h = get(gca,'Children'); % Get line handles
+set(h4(end),'LineStyle','--')
 xlabel('Axis orientation difference (radians)')
 legend(h4,h3legend)
 
@@ -401,8 +403,7 @@ subplot(2,2,4)
 plot(dist_shift,orient_diff_all_mean(1:end-1),'b',...
     [dist_shift(1) dist_shift(end)], repmat(orient_diff_all_mean(end),1,2), 'r--')
 xlabel('Registration Shift (pixels)')
-ylabel('Mean Axis orientation difference (radians)')
-
+ylabel('Mean Axis orientation difference (degrees)')
 
 
 %% Plot the above - need to do for same session with shifting...
@@ -434,3 +435,385 @@ ylabel(ax21(2),'p-value')
 %% Save workspace 
 
 % save(['neuron_reg_qc_plots_workspace-' datestr(now,1)])
+
+%% Get neuron axis ratios across all sessions
+
+clear session
+
+sesh_use = ref.G45.twoenv(1) + [2:5 10 12 16:17]; % octagon sessions 
+
+% Other possibilites
+%ref.G45.twoenv(1) + [0:7 9 10 12:17]; % square sessions %ref.G48.twoenv(1):ref.G48.twoenv(end); % 
+
+ChangeDirectory_NK(MD(sesh_use(1)));
+base_dir = pwd;
+if exist(fullfile(base_dir,'T2output.mat'),'file')
+    load(fullfile(base_dir,'T2output.mat'),'NeuronImage')
+    ROI_base = NeuronImage;
+else
+    disp('Using T1 output!!!! Update for final plots!')
+    load(fullfile(base_dir,'MeanBlobs.mat'),'BinBlobs');
+    ROI_base = BinBlobs;
+end
+
+% Load anc calc initial session PF info
+load(fullfile(base_dir, 'PlaceMaps_rot_to_std.mat'),'TMap_gauss','pval','cmperbin');
+session(1).pval = pval;
+[ ~, session(1).max_FR_location ] = get_PF_centroid(TMap_gauss, 0.9, 0);
+num_neurons = length(TMap_gauss);
+
+% Get neurons that are validly mapped across all sessions
+neuron_map_all_log = true(num_neurons,1);
+for j = 2:length(sesh_use)
+    session(j).dir = MD(sesh_use(j)).Location;
+    load(fullfile(session(j).dir, 'PlaceMaps_rot_to_std.mat'),'pval','TMap_gauss','cmperbin');
+    session(j).pval = pval;
+    load(fullfile(base_dir,['neuron_map-' MD(sesh_use(j)).Animal '-' MD(sesh_use(j)).Date '-session' num2str(MD(sesh_use(j)).Session) '.mat']))
+    [ ~, session(j).max_FR_location ] = get_PF_centroid(TMap_gauss, 0.9, 0);
+    [ mapped_ROIs, valid_neurons ] = map_ROIs(neuron_map.neuron_id, [], 0 );
+    num_neurons = length(neuron_map.neuron_id);
+    neuron_map_use = nan(num_neurons,1);
+    neuron_map_use(valid_neurons) = cell2mat(neuron_map.neuron_id(valid_neurons));
+    min_dist_bymouse{j} = get_PF_centroid_diff(session(1).max_FR_location, ...
+        session(j).max_FR_location, neuron_map_use,1); % get min dist for each pair
+    good_neurons = pval_filter_bw_sesh( session(1).pval, session(j).pval,...
+        neuron_map_use, 'filter_spec',PF_filterspec,'thresh',PF_pval_thresh);
+    
+    min_dist_bymouse_filter{j} = min_dist_bymouse{j}(good_neurons);
+    
+    % Assemble logical neuron map for all sessions to get final set of
+    % neurons active across all sessions!
+    temp = false(num_neurons,1);
+    temp(good_neurons) = true;
+    neuron_map_all_log = neuron_map_all_log & temp;
+end
+
+AllActiveMap{1} = create_AllICmask(ROI_base(neuron_map_all_log));
+
+for j = 2:length(sesh_use)
+    
+    load(fullfile(base_dir,['neuron_map-' MD(sesh_use(j)).Animal '-' MD(sesh_use(j)).Date '-session' num2str(MD(sesh_use(j)).Session) '.mat']))
+    ChangeDirectory_NK(MD(sesh_use(j)));
+    if exist(fullfile(pwd,'T2output.mat'),'file')
+        load(fullfile(pwd,'T2output.mat'),'NeuronImage')
+        ROI_reg = NeuronImage;
+    else
+        disp('Using T1 output!!!! Update for final plots!')
+        load(fullfile(pwd,'MeanBlobs.mat'),'BinBlobs');
+        ROI_reg = BinBlobs;
+    end
+    
+
+    load(fullfile(base_dir,['RegistrationInfo-' MD(sesh_use(j)).Animal '-' MD(sesh_use(j)).Date '-session' num2str(MD(sesh_use(j)).Session) '.mat']))
+    disp('Warping reg session ROIs to base session')
+    % Register all shifted neuron ROIs to base session
+    resol = 1; % Percent resolution for progress bar, in this case 10%
+    p = ProgressBar(100/resol);
+    update_inc = round(length(ROI_reg)/(100/resol)); % Get increments for updating ProgressBar
+    for ll = 1:length(ROI_reg)
+        tform_use = RegistrationInfoX.tform;
+        ROI_reg{ll} = imwarp(ROI_reg{ll}, tform_use,...
+            'OutputView', RegistrationInfoX.base_ref,'InterpolationMethod','nearest');
+        if round(ll/update_inc) == (ll/update_inc)
+            p.progress;
+        end
+
+    end
+    p.stop;
+    
+    [ mapped_ROIs, valid_neurons ] = map_ROIs(neuron_map.neuron_id, ROI_reg );
+    
+    AllActiveMap{j} = create_AllICmask(mapped_ROIs(neuron_map_all_log));
+    
+    % Get differences between centroids, axis ratio, major axis angle
+    [ cent_shift, cent_dist_shift, axratio_shift, axratio_diff_shift, orient_shift, ...
+        orient_diff_shift ] = dist_bw_reg_sessions( {ROI_base(valid_neurons), ...
+        mapped_ROIs(valid_neurons)} );
+    
+    axratio_diff_bymouse{j} = squeeze(axratio_diff_shift(1,2,:));
+    orient_diff_bymouse{j} = squeeze(orient_diff_shift(1,2,:));
+    
+    % Get overlap info for each shift
+    [overlap_ratio_temp, overlap_pixels_temp, total_pixels_temp, ~, ratio3_temp ] = ...
+        reg_calc_overlap( ROI_base(valid_neurons), mapped_ROIs(valid_neurons));
+    
+    overlap_ratio_bymouse{j} = overlap_ratio_temp;
+    
+%     disp('Getting PF centroid distances')
+%     session(j).dir = MD(sesh_use(j)).Location;
+%     load(fullfile(session(j).dir, 'PlaceMaps_rot_to_std.mat'),'TMap_gauss','pval','cmperbin');
+%     session(j).pval = pval;
+%     [ ~, session(j).max_FR_location ] = get_PF_centroid(TMap_gauss, 0.9, 0);
+%     %%% Get PF stats
+%     num_neurons = length(neuron_map.neuron_id);
+%     neuron_map_use = nan(num_neurons,1);
+%     neuron_map_use(valid_neurons) = cell2mat(neuron_map.neuron_id(valid_neurons));
+%     min_dist_bymouse{j} = get_PF_centroid_diff(session(1).max_FR_location, ...
+%         session(j).max_FR_location, neuron_map_use,1); % get min dist for each pair
+%     
+%     % Filter cells
+%     good_neurons = pval_filter_bw_sesh( session(1).pval, session(j).pval,...
+%         neuron_map_use, 'filter_spec',PF_filterspec,'thresh',PF_pval_thresh);
+%     
+    
+    
+end
+
+%% Plot the above
+figure(3)
+for j = 2:16
+   subplot(2,2,1)
+   hold on
+   ecdf(axratio_diff_bymouse{j});
+   hold off
+   
+   subplot(2,2,2)
+   hold on
+   ecdf(overlap_ratio_bymouse{j});
+   hold off
+   
+   subplot(2,2,3)
+   hold on
+   ecdf(orient_diff_bymouse{j});
+   hold off
+   
+   
+end
+
+plot_legend = arrayfun(@(a) [mouse_name_title(a.Date) ' - ' num2str(a.Session)], ...
+    MD(sesh_use(2:end)),'UniformOutput',0);
+
+% Add in shuffled values, axis titles, and legends
+subplot(2,2,1)
+hold on
+[f, x] = ecdf(axratio_diff_all{end});
+h31s = stairs(x,f);
+hold off
+set(h31s,'LineStyle','--')
+xlabel('Axis ratio difference')
+legend({plot_legend{:} 'Shuffled'})
+
+subplot(2,2,2)
+xlabel('Overlap ratio')
+legend(plot_legend)
+
+subplot(2,2,3)
+hold on
+[f, x] =  ecdf(orient_diff_all{end});
+h33s = stairs(x,f);
+set(h33s,'LineStyle','--')
+hold off
+xlabel('Orientation difference (degrees)')
+legend({plot_legend{:} 'Shuffled'})
+
+
+
+%% Save the above
+cd(base_dir)
+save neuron_qc_bymouse axratio_diff_bymouse orient_diff_bymouse overlap_ratio_bymouse axratio_diff_all orient_diff_all overlap_ratio_all
+
+%% Aggregate results across mice
+sessions_plot = 1:3;
+compare_type = 1:2; % 1 = same session, 2 = session 1 to session 2
+compare_str = {'samesession', 'session1_session2'};
+color_spec = {'r', 'b', 'g', 'y'};
+
+for k = compare_type
+    % Dump everything into one variable
+    
+    clear agg_session test
+    for j = sessions_plot
+        
+        temp = load(fullfile(MD(session_ref(j)).Location,['neuron_qc_plots_' compare_str{compare_type(k)} '_shifted.mat']),...
+            'dist_shift','min_dist_all','min_dist_all_filter','cent_dist_all',...
+            'axratio_diff_all','orient_diff_all','overlap_ratio_all', 'min_dist_all_mean','min_dist_all_filter_mean',...
+            'cent_dist_all_mean','axratio_diff_all_mean','orient_diff_all_mean');
+        agg_session(j) = temp;
+        test(j).overlap_ratio_all_mean = cellfun(@nanmean, temp.overlap_ratio_all);
+        
+    end
+    
+    [agg_session.overlap_ratio_all_mean] = deal(test.overlap_ratio_all_mean);
+    
+    aratio_shuf_plot = mean(arrayfun(@(a) a.axratio_diff_all_mean(end), agg_session));
+    orient_shuf_plot = mean(arrayfun(@(a) a.orient_diff_all_mean(end), agg_session));
+    min_dist_shuf_plot = mean(arrayfun(@(a) a.min_dist_all_mean(end), agg_session));
+    
+    % Run through and plot everything on one graph
+    
+    figure(545)
+    for j = sessions_plot
+        
+        % Plot distance between PF centroids
+        hh{1}(k) = subplot(4,2,k);
+        hold on;
+        plot(agg_session(j).dist_shift,agg_session(j).min_dist_all_filter_mean)
+        if j == max(sessions_plot)
+            plot(agg_session(j).dist_shift([1, end]), [min_dist_shuf_plot, min_dist_shuf_plot], 'k--')
+        end
+        hold off;
+        xlabel('Distance shifted (pixels)');
+        ylabel('PF centroid distance (cm)');
+        
+        % Plot overlap ratios
+        hh{2}(k) = subplot(4,2,k+2);
+        hold on;
+        plot(agg_session(j).dist_shift,agg_session(j).overlap_ratio_all_mean)
+        hold off;
+        xlabel('Distance shifted (pixels)');
+        ylabel('Neuron ROI overlap ratio');
+        
+        % Plot overlap ratios
+        hh{3}(k) = subplot(4,2,k+4);
+        hold on;
+        plot(agg_session(j).dist_shift,agg_session(j).axratio_diff_all_mean(1:end-1))
+        if j == max(sessions_plot)
+            plot(agg_session(j).dist_shift([1, end]), [aratio_shuf_plot, aratio_shuf_plot], 'k--')
+        end
+        hold off;
+        xlabel('Distance shifted (pixels)');
+        ylabel('Neuron axis ratio (shape) difference');
+        
+        % Plot overlap ratios
+        hh{4}(k) = subplot(4,2,k+6);
+        hold on;
+        plot(agg_session(j).dist_shift,agg_session(j).orient_diff_all_mean(1:end-1))
+        if j == max(sessions_plot)
+            plot(agg_session(j).dist_shift([1, end]), [orient_shuf_plot, orient_shuf_plot], 'k--')
+        end
+        hold off;
+        xlabel('Distance shifted (pixels)');
+        ylabel('Neuron Orientation Difference (radians)');
+        
+        
+    end
+    
+    % Add legends and titles
+    for j = 1:4
+        subplot(4,2,(j-1)*2+k)
+        legend(arrayfun(@(a) ['Mouse ' num2str(a)],sessions_plot,'UniformOutput',0))
+        if j == 1
+            title(mouse_name_title(compare_str{k}))
+        end
+    end
+
+end
+
+for j = 1:4
+    linkaxes(hh{j},'y')
+end
+
+%% Figure 1: PF centroid distance and Aratio diff for shifted for one mouse,
+% Then aratio diff for actual sessions for one mouse, then mean (with
+% pvalue) of PF centroid diff OR all mice combined for PF centroid shift?
+
+mouse_use = 2; 
+sesh_use = ref.G45.twoenv(1) + [2:5 10 12 16:17]; % ref.G45.twoenv(1) + [0:7 9 10 12:17]; % square sessions
+PF_plots_use = 2:length(sesh_use); %[2 7 8 9 12 13 14]; %[7 9 12 14];
+
+load(fullfile(MD(session_ref(mouse_use)).Location,'neuron_qc_plots_session1_session2_shifted.mat'));
+load(fullfile(MD(sesh_use(1)).Location,'neuron_qc_bymouse.mat'));
+
+% Define Plot legends for below
+h2legend = arrayfun(@(a) [num2str(a) ' pixels'],dist_shift,'UniformOutput',0);
+h3legend = h2legend;
+h3legend{length(h2legend) + 1} = 'Shuffled';
+plot_legend = arrayfun(@(a) [mouse_name_title(a.Date) ' - ' num2str(a.Session)], ...
+    MD(sesh_use(2:end)),'UniformOutput',0);
+
+figure(560)
+for j = 1:length(dist_shift)+1
+    
+    % Shifted PF centroid distance distances
+    subplot(2,2,1)
+    hold on
+    [f,x] = ecdf(min_dist_all{j}*cmperbin);
+    hf1(j) = stairs(x,f);
+    hold off
+    
+    % Shifted axis orientation differences
+    subplot(2,2,3);
+    hold on;
+    [f,x] = ecdf(orient_diff_all{j});
+    hf3(j) = stairs(x,f);
+    hold off
+    
+end
+
+subplot(2,2,1)
+set(hf1(end),'LineStyle','--')
+xlabel('Distance between PF centroids (cm)')
+legend(hf1,h3legend)
+title('Session 1 to Session 2 with Image Reg. Shifted')
+
+subplot(2,2,3)
+set(hf3(end),'LineStyle','--')
+xlabel('Orientation difference (degrees)')
+legend(hf3,h3legend)
+title('Session 1 to Session 2 with Image Reg. Shifted')
+
+% Plot axis orientation difference for all sessions
+for j = 2:length(orient_diff_bymouse)
+    subplot(2,2,4)
+    hold on
+    ecdf(orient_diff_bymouse{j});
+    hold off
+    
+    % Play with 'defaultAxesColorOrder' or set(gca,'ColorOrder') to get a
+    % better idea of which sessions are which!!!
+    subplot(2,2,2)
+    hold on
+    if ismember(j,PF_plots_use)
+        set(gca,'LineStyleOrder','-')
+        ecdf(min_dist_bymouse_filter{j})
+    else
+%         set(gca,'LineStyleOrder',':')
+    end
+    hold off
+end
+
+subplot(2,2,4)
+hold on
+[f, x] =  ecdf(orient_diff_all{end});
+hf4 = stairs(x,f);
+set(hf4,'LineStyle','--')
+hold off
+xlabel('Orientation difference (degrees)')
+legend({plot_legend{:} 'Shuffled'})
+title('8/28/2015 Session 1 to multiple other sessions')
+
+subplot(2,2,2)
+hold on
+[f, x] =  ecdf(min_dist_shuffle);
+hf2 = stairs(x,f);
+set(hf2,'LineStyle','--')
+hold off
+xlabel('Distance between PF centroids (cm)')
+% legend({plot_legend{:} 'Shuffled'})
+legend({plot_legend{PF_plots_use-1} 'Shuffled'})
+title('8/28/2015 Session 1 to multiple other sessions')
+
+
+
+%%% This plots only the lines for the first and last two registered
+%%% sessions for clarity.
+% for j = [2 3 15 16]
+%     subplot(2,2,2)
+%     hold on
+%     ecdf(orient_diff_bymouse{j});
+%     hold off
+% end
+% hold on
+% [f, x] =  ecdf(orient_diff_all{end});
+% hf2 = stairs(x,f);
+% set(hf2,'LineStyle','--')
+% hold off
+% xlabel('Orientation difference (degrees)')
+% legend({plot_legend{[1 2 14 15]} 'Shuffled'})
+
+%%% Get neurons active in EVERY session
+
+
+
+
+
