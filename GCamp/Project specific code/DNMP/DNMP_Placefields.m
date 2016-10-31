895d42c2a0d4fd2f1afb4f0372c97fd43258af01
nkinsky <kinsky@bu.edu>
2016-10-31 15:29:41 -0400

    misc adds

--- GCamp/Project specific code/DNMP/DNMP_Placefields.m
+++ GCamp/Project specific code/DNMP/DNMP_Placefields.m
@@ -3,14 +3,23 @@ function [] = DNMP_Placefields( session_struct, varargin )
 %   Detailed explanation goes here
 
 stats_only = 0;
+neuron_input = 'T2output.mat';
+cmperbin = 2;
+minspeed = 7;
+NumShuffles = 10; % For starters
 for j = 1:length(varargin)
    if strcmpi(varargin{j},'stats_only') 
        stats_only = varargin{j+1};
    end
+   if strcmpi(varargin{j},'neuron_input')
+       neuron_input = varargin{j+1};
+       disp(['Using neuronal data from file: ' neuron_input])
+   end
+   if strcmpi(varargin{j},'minspeed')
+       minspeed = varargin{j+1};
+       disp(['User set minspeed to ' num2str(minspeed) ' cm/s'])
+   end
 end
-
-cmperbin = 1;
-NumShuffles = 500; % For starters
 
 % SaveNames
 save_names = {'PlaceMapsv2_onmaze.mat','PlaceMapsv2_forced.mat','PlaceMapsv2_free.mat',...
@@ -28,12 +37,12 @@ if stats_only == 0
         ChangeDirectory_NK(session_struct(j));
         exc_frames = load('exclude_frames.mat'); % Load frames to exclude for each type of trials - must run DNMP_parse_trials beforehand to get
         
-        for k = 1:1 %length(save_names)
+        for k = 1:length(save_names)
             % Get on-maze PFs
             disp(['Running CalculatePlaceFields for ' name_append{k} ' session.'])
             CalculatePlacefields(room,'exclude_frames_raw',exc_frames.(exc_frames_type{k}),...
-                'alt_inputs','T2output.mat','man_savename',save_names{k},...
-                'half_window',0,'minspeed',3,'cmperbin',cmperbin,...
+                'alt_inputs',neuron_input,'man_savename',save_names{k},...
+                'half_window',0,'minspeed',minspeed,'cmperbin',cmperbin,...
                 'NumShuffles',NumShuffles,'calc_half',1);
         end
         
@@ -46,9 +55,9 @@ end
 
 %% Get stats
 
-for k = 1:1 % length(save_names)
+for k = 1:length(save_names)
     disp(['Running PFstats for ' name_append{k} ' session.'])
-    PFstats(0, 'alt_file_use', save_names{k}, name_append{k})
+    PFstats(0, 'alt_file_use', save_names{k}, ['v2_' name_append{k}])
 end
 
 end
--- GCamp/Project specific code/DNMP/DNMP_compare_TMap.m
+++ GCamp/Project specific code/DNMP/DNMP_compare_TMap.m
@@ -1,7 +1,12 @@
 % DNMP figure export
 
+plot_pval = false;
 
-sesh = MD(180);
+sesh = MD(300);
+IChack = true;
+if IChack
+    plot_pval = false;
+end
 
 trial_types = {'Forced L', 'Forced R', 'Free L', 'Free R'}; % {'Forced','Free'};
 file_name_append = {'_forced_left.mat', '_forced_right.mat', '_free_left.mat', '_free_right.mat'}; %{'_forced.mat','_free.mat'}; % Must match block types above
@@ -10,43 +15,93 @@ plot_file = 'PFcompare_wpvalues';
 %% Plot actual comparisons
 
 % load all the variables
+if ~IChack
+    load(fullfile(sesh.Location,'NormTraces.mat'),'trace');
+    load(fullfile(sesh.Location,'T2output.mat'),'FT');
+elseif IChack
+    disp('Using data from ICout_hack.mat and NormTraces_hack.mat')
+    load(fullfile(sesh.Location,'NormTraces_hack.mat'),'trace');
+%     load(fullfile(sesh.Location,'ICout_hack.mat'),'FT');
+end
+load on_off_ind.mat
+
 trials = load(fullfile(sesh.Location,['PlaceMapsv2' file_name_append{1}]),'TMap_gauss',...
-    'RunOccMap','pval');
+    'RunOccMap','pval','isrunning','FToffset','FT');
+FT = trials(1).FT;
+FToffset = trials(1).FToffset;
+isrunning_all = false(size(trials(1).isrunning));
 NumNeurons = length(trials(1).TMap_gauss);
+NumFrames = size(FT,2);
 for k = 2:length(trial_types)
     trials(k) = load(fullfile(sesh.Location,['PlaceMapsv2' file_name_append{k}]),'TMap_gauss',...
-        'RunOccMap','pval');
+        'RunOccMap','pval','isrunning','FToffset','FT');
+    isrunning_all = isrunning_all | trials(k).isrunning;
 end
+
+on_maze_log = false(1,NumFrames); off_maze_log = false(1,NumFrames);
+on_maze_log(on_maze_ind) = true;
+off_maze_log(off_maze_ind) = true;
 
 for k = 1:length(trial_types)
     load(fullfile(sesh.Location,['PFstatsv2' file_name_append{k}]),'PFnumhits');
     trials(k).PFnumhits = PFnumhits;
 end
 
+trace_use = trace(:,FToffset-1:end);
+
 figure(543)
+set(gcf,'Position',[1 41 1920 964]);
 cm = colormap('jet');
-for j = 1184:NumNeurons
+neurons_plot = [17 18 19 20 23 25 26 27 28 31 36 39 42 44 47 54 55 56];
+for j = 1:length(neurons_plot)
     
     % Refresh figure every 100 to prevent slowdowns
-    if round(j/100) == (j/100)
+    if round(j/50) == (j/50)
         close 543
         figure(543)
+        set(gcf,'Position',[1 41 1920 964]);
     end
     for k = 1:length(trials)
         if length(trials) == 2
-            subplot(1,2,k)
-        elseif length(trials) == 4
             subplot(2,2,k)
+        elseif length(trials) == 4
+            sub_ref = { [ 1 2 5 6] [3 4 7 8] [9 10 13 14] [ 11 12 15 16]};
+            ha{k} = subplot(5,4,sub_ref{k});
         end
-        [~, nan_map] = make_nan_TMap(trials(k).RunOccMap,trials(k).TMap_gauss{j},...
+        [~, nan_map] = make_nan_TMap(trials(k).RunOccMap,trials(k).TMap_gauss{neurons_plot(j)},...
             'perform_smooth',1);
+        scale_max(k) = nanmax(nan_map(:));
         imagesc_nan(nan_map,cm,[1 1 1]);
-        title(['Neuron ' num2str(j) ' ' trial_types{k} ' Trials'])
-        xlabel(['Number hits = ' num2str(max(trials(k).PFnumhits(j,:)))])
-        ylabel(['pval = ' num2str(1 - trials(k).pval(j))])
+        title(['Neuron ' num2str(neurons_plot(j)) ' ' trial_types{k} ' Trials'])
+        axis off
+%         xlabel(['Number hits = ' num2str(max(trials(k).PFnumhits(j,:)))])
+        if plot_pval
+            ylabel(['pval = ' num2str(1 - trials(k).pval(neurons_plot(j)))])
+        end
     end
     
-     export_fig(plot_file,'-pdf','-append')
+    % Match heat map scales so that the same color means the same transient
+    % probability in each map
+    scale_max = nanmax(scale_max);
+    if isnan(scale_max) || scale_max == 0
+        scale_max = 1;
+    end
+    clim_range = [-scale_max/50 scale_max];
+    for k = 1:length(trials)
+       set(ha{k},'CLim',clim_range);
+    end
+    
+    
+    % Plot trace
+    if length(trials) == 2
+        subplot(2,2,[3 4])
+    elseif length(trials) == 4
+        subplot(5,4,17:20)
+    end
+    plot(1:NumFrames,trace_use(neurons_plot(j),:),'b-',find(FT(neurons_plot(j),:) & ~isrunning_all),trace_use(neurons_plot(j),FT(neurons_plot(j),:) & ~isrunning_all),'r.',...
+        find(FT(neurons_plot(j),:) & isrunning_all & on_maze_log),trace_use(neurons_plot(j),FT(neurons_plot(j),:) & isrunning_all & on_maze_log),'g.');
+    
+    export_fig(plot_file,'-pdf','-append')
     
 end
 
