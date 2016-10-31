7cbb00a185ee93bc0a314f038472e76b350328b5
Nat <kinsky@bu.edu>
2016-10-30 20:44:31 -0400

    initial commit

---
+++ GCamp/Project specific code/DNMP/DNMP_mean_image_compare.m
@@ -0,0 +1,7 @@
+% DNMP task compare mean frames
+
+%% 1 - Calculate Mean Frame for R/L Forced Trials, and for odd and even forced trials
+
+%% 2 - Calculate Mean Frame for R/L Free Trials , and for odd and even free trials
+
+%% 3 - Calculate correlation between 1 and 2, and within 1/2 odd v even
