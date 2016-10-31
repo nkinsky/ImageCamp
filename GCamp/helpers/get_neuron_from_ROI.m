7c968bace341d7f700ede5caeed1109d3799b8b3
nkinsky <kinsky@bu.edu>
2016-10-31 15:30:06 -0400

    bugfixes

--- GCamp/helpers/get_neuron_from_ROI.m
+++ GCamp/helpers/get_neuron_from_ROI.m
@@ -1,4 +1,4 @@
-function [ neuron_id ] = get_neuron_from_ROI(NeuronImage, axes_handle )
+function [ neuron_id ] = get_neuron_from_ROI(NeuronImage, axes_handle, varargin )
 %UNTITLED Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -11,13 +11,40 @@ if nargin < 2
     axes_handle = gca;
 end
 
+alt_xy = false; % default
+for j = 1:length(varargin)
+   if strcmpi('alt_xy',varargin{j})
+       alt_xy = true;
+       x_alt = varargin{j+1}(1);
+       y_alt = varargin{j+1}(2);
+   end
+end
+    
+
 % Get neuron centroids
-temp = cellfun(@(a) regionprops(a,'Centroid'),NeuronImage);
-centroids = cat(1,temp.Centroid);
+try
+    temp = cellfun(@(a) regionprops(a,'Centroid'),NeuronImage);
+    centroids = cat(1,temp.Centroid);
+catch
+    centroids = [];
+    for j = 1:length(NeuronImage)
+        temp = regionprops(NeuronImage{j},'Centroid','Area');
+        if length(temp) == 1
+            centroids = [centroids; temp.Centroid];
+        else
+            [~, ii] = max(cat(1,temp.Area));
+            centroids = [centroids; temp(ii).Centroid];
+        end
+    end
 
 % Get location of neuron centroid from user input
-axes(axes_handle);
-[x,y] = ginput(1);
+if ~alt_xy
+    axes(axes_handle);
+    [x,y] = ginput(1);
+elseif alt_xy % get location from varargin input
+    x = x_alt;
+    y = y_alt;
+end
 
 % Find closest neuron
 xy_diff = centroids - repmat([x,y],NumNeurons,1);
@@ -26,11 +53,13 @@ dist_all = sqrt(xy_diff(:,1).^2 + xy_diff(:,2).^2);
 [~, neuron_id] = min(dist_all);
 
 % Check
-b = bwboundaries(NeuronImage{neuron_id},'noholes');
-axes(axes_handle)
-hold on
-plot(b{1}(:,2),b{1}(:,1),'r.')
-hold off
+if ~isempty(axes_handle)
+    b = bwboundaries(NeuronImage{neuron_id},'noholes');
+    axes(axes_handle)
+    hold on
+    plot(b{1}(:,2),b{1}(:,1),'r.')
+    hold off
+end
 
 end
 
