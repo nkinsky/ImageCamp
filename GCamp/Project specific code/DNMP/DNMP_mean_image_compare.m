8290711114d33e591076fa2dfb6a7c9c0dcd318d
Nat <kinsky@bu.edu>
2016-10-30 22:15:41 -0400

    updates - should probably make this a function

--- GCamp/Project specific code/DNMP/DNMP_mean_image_compare.m
+++ GCamp/Project specific code/DNMP/DNMP_mean_image_compare.m
@@ -1,6 +1,13 @@
 % DNMP task compare mean frames
 
+%% 0 - Get frames to include for each trial type/direction
+
+
+%% 0.1 - Identify epochs for each trial type/direction
+
 %% 1 - Calculate Mean Frame for R/L Forced Trials, and for odd and even forced trials
+% For loop through through each epoch, concatenate frames along dim 3,
+% write subfunction for this
 
 %% 2 - Calculate Mean Frame for R/L Free Trials , and for odd and even free trials
 
