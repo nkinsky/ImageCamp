7259201bcc2b5318dd88d629ee29685136d38701
nkinsky <kinsky@bu.edu>
2017-01-28 14:49:43 -0500

    start of final updates

--- GCamp/reg_qc_plot.m
+++ GCamp/reg_qc_plot.m
@@ -12,11 +12,19 @@ p.addRequired('avg_corr', @isnumeric)
 p.addOptional('h', 0, @ishandle)
 p.addParameter('plot_shuf', 0, @(a) isnumeric(a) || a == 0 || a == 1 || a == 2); % 0 = normal, 1 = shuffled, 2 = shifted
 p.addParameter('multi_sesh', false, @(a) islogical(a) || a == 0 || a == 1);
+p.addParameter('dist_edges', 0:0.1:3, @isnumeric);
+p.addParameter('orient_edges', -180:10:180, @isnumeric);
 p.parse(centroid_dist, orient_diff, avg_corr, varargin{:});
 
 h = p.Results.h;
 multi_sesh = p.Results.multi_sesh;
 plot_shuf = p.Results.plot_shuf;
+dist_edges = p.Results.dist_edges;
+orient_edges = p.Results.orient_edges;
+
+dist_centers = mean([dist_edges(1:end-1); dist_edges(2:end)]);
+orient_centers = mean([orient_edges(1:end-1); orient _edges(2:end)]);
+
 
 if h == 0
     h = figure;
@@ -44,7 +52,7 @@ switch multi_sesh
         if ~isempty(orient_diff)
             subplot(1,2,2)
             hold on
-            plotfun(abs(orient_diff), plot_shuf)
+            ecdfplotfun(abs(orient_diff), plot_shuf)
             xlabel('Absolute Orientation Difference (\theta, degrees)')
             ylabel('F(\theta)');
             hold off
@@ -55,7 +63,7 @@ switch multi_sesh
             subplot(1,2,1)
             hold on
             % histogram(centroid_dist,0:0.25:10);
-            plotfun(centroid_dist, plot_shuf);
+            ecdfplotfun(centroid_dist, plot_shuf);
             xlabel('Centroid Distance (cd)')
             % ylabel('Count')
             ylabel('F(cd)')
@@ -65,7 +73,7 @@ switch multi_sesh
         if ~isempty(orient_diff)
             subplot(1,2,2)
             hold on
-            plotfun(abs(orient_diff), plot_shuf)
+            ecdfplotfun(abs(orient_diff), plot_shuf)
             xlabel('Absolute Orientation Difference (\theta, degrees)')
             ylabel('F(\theta)');
             hold off
@@ -85,7 +93,7 @@ end
 end
 
 %% ecdf Plot sub-function
-function [] = plotfun(a, shuf)
+function [] = ecdfplotfun(a, shuf)
 
 if shuf == 0
     ecdf(a);
