4c7e515fa7e39adba5c318bc39e1ca8643769d8a
nkinsky <kinsky@bu.edu>
2017-02-01 15:46:03 -0500

    enhancement - take partial MD input and complete it

--- GCamp/helpers/ChangeDirectory_NK.m
+++ GCamp/helpers/ChangeDirectory_NK.m
@@ -1,4 +1,4 @@
-function [ dirstr ] = ChangeDirectory_NK(MD_in,change_dir_flag)
+function [ dirstr, MD_out ] = ChangeDirectory_NK(MD_in,change_dir_flag)
 % dirstr = ChangeDirectory_NK(MD_in,change_dir_flag)
 %
 % Lazy man's ChangeDirectory - takes only the appropriate session from MD as
@@ -11,7 +11,7 @@ if ~exist('change_dir_flag','var')
     change_dir_flag = 1; % default value
 end
 
-dirstr = ChangeDirectory(MD_in.Animal,MD_in.Date,MD_in.Session,change_dir_flag);
+[dirstr, MD_out] = ChangeDirectory(MD_in.Animal,MD_in.Date,MD_in.Session,change_dir_flag);
 
 end
 
