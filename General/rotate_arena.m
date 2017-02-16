7053cea3475f929e21d81e7130b7fceafa36eceb
nkinsky <kinsky@bu.edu>
2017-02-16 13:48:15 -0500

    enhancement

--- General/rotate_arena.m
+++ General/rotate_arena.m
@@ -1,10 +1,10 @@
-function [ x_rot, y_rot ] = rotate_arena( x, y, rot)
-%[ x_rot, y_rot ] = rotate_xy( x, y, rot ) 
+function [ x_rot, y_rot ] = rotate_arena( x, y, rot_deg)
+%[ x_rot, y_rot ] = rotate_xy( x, y, rot_deg ) 
 %
 %   INPUTS
 %   x and y: arrays of position data that you wish to rotate
 %
-%   rot: in degress, how much you wish to rotate the data.   There are 
+%   rot_deg: in degress, how much you wish to rotate the data.   There are 
 %   three options: -90 (CW), 90 (CCW), and 180
 %
 %   OUTPUTS
@@ -15,29 +15,35 @@ function [ x_rot, y_rot ] = rotate_arena( x, y, rot)
 x_orig = min(x);
 y_orig = min(y);
 
-% perform the rotation
-if rot == 90 % CW 
-    x_rot = -y;
-    y_rot = x;
-elseif rot == - 90 % CCW  
-    x_rot = y;
-    y_rot = -x;    
-elseif rot == 180  
-    x_rot = -x;
-    y_rot = -y;  
-elseif rot == 0
-    x_rot = x;
-    y_rot = y;
-else
-    error('You have not entered a valid argument for "rot"')
-end
+%% perform the rotation
+% if rot_deg == 90 % CW 
+%     x_rot = -y;
+%     y_rot = x;
+% elseif rot_deg == - 90 % CCW  
+%     x_rot = y;
+%     y_rot = -x;    
+% elseif rot_deg == 180  
+%     x_rot = -x;
+%     y_rot = -y;  
+% elseif rot_deg == 0
+%     x_rot = x;
+%     y_rot = y;
+% else
+%     error('You have not entered a valid argument for "rot"')
+% end
 
-% Translate back to origin (either original or specified)
+%% Try another tact
+rot_mat = [cosd(rot_deg) -sind(rot_deg); sind(rot_deg) cosd(rot_deg)];
+xy_rot = rot_mat*[x; y];
+x_rot = xy_rot(1,:);
+y_rot = xy_rot(2,:);
+
+%% Translate back to origin (either original or specified)
 x_rot_min = min(x_rot); dx = x_orig - x_rot_min;
 y_rot_min = min(y_rot); dy = y_orig - y_rot_min;
 
 x_rot = x_rot + dx;
-y_rot = y_rot + dx;
+y_rot = y_rot + dy;
 
 end
 
