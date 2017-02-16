7a0e0c1a5ddd865b5ce14e81c658a00f4971d1d4
nkinsky <kinsky@bu.edu>
2017-02-16 13:50:32 -0500

    enhancement

--- tracking/draw_manual_limits.m
+++ tracking/draw_manual_limits.m
@@ -1,4 +1,4 @@
-function [ x_manual, y_manual, ind_keep ] = draw_manual_limits( x,y )
+function [ x_manual, y_manual, ind_keep ] = draw_manual_limits( x,y, env_label )
 %[ x_manual, y_manual ] = draw_manual_limits( x,y )
 %   Plots x and y and has you draw a rectangle over them defining which
 %   points to keep and which to discard
@@ -6,12 +6,16 @@ function [ x_manual, y_manual, ind_keep ] = draw_manual_limits( x,y )
 %   INPUTS
 %   x,y: input position data
 %
+%   env_label: something to ID the environment with (e.g 'alternation
+%   track')
+%
 %   OUTPUTS
 %   x_manual,y_manual: output data that only includes points from x and y
 %   that are within the rectangle you drew.
 %
 %   ind_keep: index to the points in x and y that correspond to x_manual,
 %   y_manual
+
 
 % Plot x and y
 figure(1234)
@@ -22,6 +26,7 @@ ok = 'n';
 
 while strcmpi(ok,'n')
     title('Draw rectangle around points you wish to consider for arena alignment!')
+    xlabel(env_label)
     disp('Draw rectangle around points you wish to consider for arena alignment!')
     rect = getrect(1234); %[xmin ymin width height]
     figure(1234)
