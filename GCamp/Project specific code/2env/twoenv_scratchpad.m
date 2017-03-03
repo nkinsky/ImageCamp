073ee7003d68d5c83bd4d0355cc68a8595866393
nkinsky <kinsky@bu.edu>
2017-03-03 09:07:35 -0500

    bugfixex

--- GCamp/Project specific code/2env/twoenv_scratchpad.m
+++ GCamp/Project specific code/2env/twoenv_scratchpad.m
@@ -63,23 +63,23 @@ for j = 1:50
 end
 
 %% Two-env rotation analysis
-sesh_use = G31_oct;
-limits_use = G45_manual_limits;
+sesh_use = cat(2,G30_square, G31_square);
+limits_use = G31_manual_limits;
 rot_use = 0:15:345; % for circle % 0:90:270; % for square  
 batch_rot_arena(sesh_use(1), sesh_use(2:end), rot_use, logical(limits_use));
 
 %% GC above
 figure(35)
 n = 1;
-rot_check = [90 270]; % [45 330];
-[dirstr, ~]  = ChangeDirectory_NK(sesh_use(j),0);
+rot_check = [90]; %[90 270]; % 
+[dirstr, ~]  = ChangeDirectory_NK(sesh_use(1),0);
 load(fullfile(dirstr,'Pos_align_rot0.mat'),'xmin','xmax','ymin','ymax');
 for j = 1:length(sesh_use)
     
     [dirstr, temp]  = ChangeDirectory_NK(sesh_use(j),0);
-    for k = 1:2
+    for k = 1:length(rot_check)
         ax(n) = subplot(4,6,n);
-        load(fullfile(dirstr,['Pos_align_rot' num2str(rot_check(k)) '.mat']),'x_adj_cm','y_adj_cm');
+        load(fullfile(dirstr,['Pos_align_rot' num2str(rot_check(k)) '.mat']),'x_adj_cm','y_adj_cm','xmin','xmax','ymin','ymax');
         plot(x_adj_cm, y_adj_cm)
         xlabel(temp.Env)
         title([mouse_name_title(temp.Date ) ' - session ' num2str(temp.Session)])
@@ -101,7 +101,7 @@ for j = 1:length(sesh_use)
 end
 
 %% two-env circle-to-square comparison - NOTE MAY NEED TO CORRECT G30 limits for this and RE-RUN all PFs!
-sesh_use = G48_botharenas; limits_use = G48_both_manual_limits;
+sesh_use = cat(2,G30_botharenas, G31_botharenas); limits_use = G48_both_manual_limits;
 rot_array = 0:15:345;
 batch_rot_arena(sesh_use(1), sesh_use(2:end), rot_array, logical(limits_use), 'circ2square', true);
 
@@ -112,7 +112,7 @@ neuron_reg_batch(sesh_use(1), sesh_use(2:end), 'name_append', '_trans');
 figure(35)
 n = 1;
 rot_check = [45 330];
-[dirstr, temp]  = ChangeDirectory_NK(sesh_use(j),0);
+[dirstr, temp]  = ChangeDirectory_NK(sesh_use(1),0);
 load(fullfile(dirstr,'Pos_align_trans_rot0.mat'),'xmin','xmax','ymin','ymax');
 for j = 1:length(sesh_use)
     
@@ -129,7 +129,7 @@ for j = 1:length(sesh_use)
         end
     elseif ~isempty(regexpi(temp.Env,'square'))
         ax(n) = subplot(4,6,n);
-        load(fullfile(dirstr,'Pos_align_trans_rot0.mat'),'x_adj_cm','y_adj_cm');
+        load(fullfile(dirstr,'Pos_align_trans_rot180.mat'),'x_adj_cm','y_adj_cm');
         plot(x_adj_cm, y_adj_cm)
         xlabel(temp.Env)
         title([mouse_name_title(temp.Date) ' - session ' num2str(temp.Session)])
@@ -141,20 +141,100 @@ end
 linkaxes(ax);
 
 %% Run immediately after above
+sesh_use = cat(2, G45_square(2:end), G48_square, G30_square, G31_square);
+rot_array_circle = 0:15:345;
+rot_array_square = 90:90:270;
 for j = 1:length(sesh_use)
     [dirstr, full_sesh] = ChangeDirectory(sesh_use(j).Animal, sesh_use(j).Date, sesh_use(j).Session);
-    disp(['Running Circle2Square Rotated Placefield Analysis on ' sesh_use(j).Animal ' - ' sesh_use(j).Date ' - session ' num2str(sesh_use(j).Session)])
+    disp(['Running Circle2Square Rotated Placefield Analysis on ' sesh_use(j).Animal ...
+        ' - ' sesh_use(j).Date ' - session ' num2str(sesh_use(j).Session)])
+    
     if ~isempty(regexpi(full_sesh.Env,'octagon'))
-        for k = 1:length(rot_array)
-            Placefields(sesh_use(j),'minspeed',1,'name_append', ['_trans_rot' num2str(rot_array(k))],'Pos_data', ['Pos_align_trans_rot' num2str(rot_array(k))]);
-            PlacefieldStats(sesh_use(j),'name_append',['_trans_rot' num2str(rot_array(k))]);
-        end
+        rot_array_use = rot_array_circle;
     elseif ~isempty(regexpi(full_sesh.Env,'square'))
-        Placefields(sesh_use(j),'minspeed',1,'name_append', '_trans_rot0','Pos_data', 'Pos_align_trans_rot0.mat');
-        PlacefieldStats(sesh_use(j),'name_append','_trans_rot0');
+        rot_array_use = rot_array_square;
+    end
+    
+    for k = 1:length(rot_array_use)
+        Placefields(sesh_use(j),'minspeed',1,'name_append', ['_trans_rot' num2str(rot_array_use(k))],...
+            'Pos_data', ['Pos_align_trans_rot' num2str(rot_array_use(k))]);
+        PlacefieldStats(sesh_use(j),'name_append',['_trans_rot' num2str(rot_array_use(k))]);
     end
 end
 
 %% Fix batch_align_pos
 sesh_use = G31_botharenas;
 
+%% Plot TMaps for all arenas with ideally aligned rotations
+sesh_use = G45_botharenas;
+rot_use = G45_both_best_angle;
+trans = false;
+
+base_dir = ChangeDirectory(sesh_use(1).Animal, sesh_use(1).Date, ...
+    sesh_use(1).Session, 0);
+load(fullfile(base_dir,'batch_session_map_trans.mat'));
+batch_map = batch_session_map;
+
+disp('Loading TMaps for all sessions')
+num_sessions = length(sesh_use);
+num_neurons = size(batch_session_map.map,1);
+TMap_rot = cell(1,num_sessions);
+for j = 1:num_sessions
+    dirstr = ChangeDirectory(sesh_use(j).Animal, sesh_use(j).Date, ...
+        sesh_use(j).Session, 0);
+    if ~isempty(regexpi(dirstr,'oct')) && trans 
+        load(fullfile(dirstr,['Placefields_trans_rot' num2str(rot_use(j)) '.mat']),'TMap_gauss');
+    elseif ~isempty(regexpi(dirstr,'square')) || (~isempty(regexpi(dirstr,'oct')) && ~trans)
+        load(fullfile(dirstr,['Placefields_rot' num2str(rot_use(j)) '.mat']),'TMap_gauss');
+    end
+    TMap_rot{j} = TMap_gauss;
+end
+
+figure
+cm = colormap('jet');
+TMap_nada = nan(size(TMap_rot{1}{1}));
+% TMap_nada(~isnan(TMap_rot{1}{1})) = 0;
+for j = 1:num_neurons
+    for k = 1:num_sessions
+        neuron_use = batch_session_map.map(j,k+1);
+        subplot_auto(num_sessions,k);
+        if ~isnan(neuron_use) && neuron_use ~= 0
+           imagesc_nan(TMap_rot{k}{neuron_use}, cm, [1 1 1]);
+           title(num2str(k))
+        else
+            imagesc_nan(TMap_nada, cm, [1 1 1]);
+        end
+    end
+    waitforbuttonpress
+end
+
+%% Add in all rotations to square sessions to circ2square alignments
+sesh_use = G31_square;
+manual_limits = G31_manual_limits;
+rot_array = 90:90:270;
+reg_sesh_full = sesh_use(1);
+man_limits_full = [];
+name_append_full = cell(1,length(sesh_use)*length(rot_array)+1);
+name_append_full{1} = '_trans_rot0';
+rot_full = [];
+n = 1;
+for j = 1:length(sesh_use)
+    rot_to_std = get_rot_from_db(sesh_use(j));
+    for k = 1:length(rot_array)
+        reg_sesh_full(n) = sesh_use(j);
+        man_limits_full(n) = G48_manual_limits(j);
+        rot_full(n) = rot_array(k) + rot_to_std;
+        name_append_full{n+1} = ['_trans_rot' num2str(rot_array(k))];
+        n = n + 1;
+    end
+end
+
+%%% NRK - need to add in the appropriate value for each of the parameters
+%%% above to make it the appropriate length
+man_limits_full = logical([manual_limits(1) man_limits_full]);
+rot_full = [0 rot_full];
+rot_full(rot_full >= 360) = rot_full(rot_full >= 360) - 360;
+
+batch_align_pos(sesh_use(1), reg_sesh_full,'skip_skew_fix', true, 'rotate_data', rot_full,...
+    'manual_limits', man_limits_full, 'name_append', name_append_full, ...
+    'suppress_output', true, 'skip_trace_align', true, 'base_adjust', false);
