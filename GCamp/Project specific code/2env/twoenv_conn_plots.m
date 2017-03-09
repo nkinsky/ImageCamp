231e733066e97b68cbb99fe6b90e739e17ea6009
Nat <kinsky@bu.edu>
2017-03-09 16:50:31 -0500

    initial commit

---
+++ GCamp/Project specific code/2env/twoenv_conn_plots.m
@@ -0,0 +1,13 @@
+function [] = twoenv_conn_plots( sessions )
+% twoenv_conn_plots( session )
+%   Scrolls through plots from each connected session. 
+
+% First need to go through each mouse and run Placefields_half using
+% specific indices as the halfway point
+
+% Then, just go through and plot each!
+num_sessions
+
+
+end
+
