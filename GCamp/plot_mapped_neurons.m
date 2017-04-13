3e6946f196e5fb54dcdc53c1a4805e39e93e3770
nkinsky <kinsky@bu.edu>
2017-04-12 20:07:32 -0400

    added output

--- GCamp/plot_mapped_neurons.m
+++ GCamp/plot_mapped_neurons.m
@@ -1,8 +1,11 @@
-function [ ] = plot_mapped_neurons( Animal, Base_Date, Base_Session, Reg_Date, Reg_Session)
-% plot_mapped_neurons( Animal, Base_Date, Base_Session, Reg_Date, Reg_Session)
+function [n1, n2, nboth ] = plot_mapped_neurons( Animal, Base_Date, Base_Session, Reg_Date, Reg_Session)
+% [n1, n2, nboth] = plot_mapped_neurons( Animal, Base_Date, Base_Session, Reg_Date, Reg_Session)
 %   Plots each session's neuron ROIs overlaid on the other.  Good for
 %   identifying good/bad registrations.  Same as plot_mapped_neurons2 but
 %   with different inputs.
+%
+%   n1, n2, nboth: # cells active in session 1 only, session 2 only, and
+%   both
 
 base_dir = ChangeDirectory(Animal, Base_Date, Base_Session, 0);
 load(fullfile(base_dir,'FinalOutput.mat'),'NeuronImage');
@@ -29,15 +32,20 @@ imagesc(sesh(1).AllNeuronMask + 2*sesh(2).AllNeuronMask);
 hc = colorbar;
 title('Red outline = same neuron across sessions')
 hold on
+nboth = 0;
 for j = 1:length(neuron_id)
     nid = neuron_id{j};
     if ~isempty(nid) && ~isnan(nid)
         b1 = bwboundaries( sesh(1).NeuronImage_reg{j},'noholes');
         b2 = bwboundaries(sesh(2).NeuronImage_reg{nid},'noholes');
         plot(b1{1}(:,2),b1{1}(:,1),'r',b2{1}(:,2),b2{1}(:,1),'r');
+        nboth = nboth + 1;
     end
 end
 hold off
+
+n1 = length(sesh(1).NeuronImage_reg) - nboth;
+n2 = length(sesh(2).NeuronImage_reg) - nboth;
 
 hc.Ticks = [1 2 3]; 
 hc.TickLabels = {'Session 1 only' 'Session 2 only' 'Overlap'};
