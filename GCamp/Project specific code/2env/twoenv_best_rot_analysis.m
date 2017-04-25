64192d8ea8b68ade708fb8ec20efbc4e37afac9c
nkinsky <kinsky@bu.edu>
2017-04-24 21:35:08 -0400

    updates

--- GCamp/Project specific code/2env/twoenv_best_rot_analysis.m
+++ GCamp/Project specific code/2env/twoenv_best_rot_analysis.m
@@ -21,29 +21,37 @@ end
 num_sessions = length(sesh_use);
 corr_mat = cell(num_sessions, num_sessions);
 shuffle_mat = cell(num_sessions, num_sessions);
-p = ProgressBar((num_sessions-1)*(num_sessions-2)/2);
-for j = 1:num_sessions-1
+if circ2square_flag
+    p = ProgressBar(num_sessions^2/2);
+else
+    p = ProgressBar((num_sessions)*(num_sessions-1)/2);
+end
+% Don't redo comparions for square-square or circle-circle, but compare all
+% square to all circle sessions
+% if circ2square_flag; end_ind = num_sessions; else; end_ind = num_sessions-1; end 
+for j = 1:num_sessions-1 %1:end_ind
     [~, struct1] = ChangeDirectory_NK(sesh_use(j));
-   for k = j+1:num_sessions
-       [~, struct2] = ChangeDirectory_NK(sesh_use(k));
-       
-       % Only perform comparison between two different structures if
-       % circ2square analysis is indicated.
-       if circ2square_flag && (~isempty(regexpi(struct1.Env,'square')) && ~isempty(regexpi(struct2.Env,'square')) || ...
-               ~isempty(regexpi(struct1.Env,'octagon')) && ~isempty(regexpi(struct2.Env,'octagon')))
-          continue  % Skip to next session if between two same shape sessions 
-          
-       end
-                  
-       % Get angle between the two sessions and
-       rot_use = best_angle(k) - best_angle(j);
-       if rot_use < 0; rot_use = rot_use + 360; end
-       
-       [corr_mat{j,k}, ~, shuffle_mat{j,k}] = corr_rot_analysis( sesh_use(j), sesh_use(k), ...
-           batch_session_map, rot_use, 'num_shuffles', num_shuffles, ...
-           'trans', circ2square_flag );
-       
-       p.progress;
+%     if circ2square_flag; start_ind = 1; else; start_ind = j+1; end
+    for k = j+1:num_sessions %start_ind:num_sessions
+        [~, struct2] = ChangeDirectory_NK(sesh_use(k));
+        
+        % Only perform comparison between two different structures if
+        % circ2square analysis is indicated.
+        if circ2square_flag && (~isempty(regexpi(struct1.Env,'square')) && ~isempty(regexpi(struct2.Env,'square')) || ...
+                ~isempty(regexpi(struct1.Env,'octagon')) && ~isempty(regexpi(struct2.Env,'octagon')))
+            continue  % Skip to next session if between two same shape sessions
+            
+        end
+        
+        % Get angle between the two sessions and
+        rot_use = best_angle(k) - best_angle(j);
+        if rot_use < 0; rot_use = rot_use + 360; end
+        
+        [corr_mat{j,k}, ~, shuffle_mat{j,k}] = corr_rot_analysis( sesh_use(j), sesh_use(k), ...
+            batch_session_map, rot_use, 'num_shuffles', num_shuffles, ...
+            'trans', circ2square_flag );
+        
+        p.progress;
        
    end
 end
