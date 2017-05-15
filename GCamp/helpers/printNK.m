9dc48830834f137369f59b8322a1a10dc94b5952
nkinsky <kinsky@bu.edu>
2017-05-14 21:03:19 -0400

    bugfix - proper plotting for large number of data points

--- GCamp/helpers/printNK.m
+++ GCamp/helpers/printNK.m
@@ -1,24 +1,44 @@
-function [  ] = printNK( filename, location )
-% printNK( filename, location )
+function [  ] = printNK( filename, location, hfig )
+% printNK( filename, location, hfig )
 %   Prints current figure to filename in location on NORVAL as a pdf with '-bestfit'
 %   option specified.
 %
 %   Location: pwd if unspecified, see below for other options.
+%
+%   hfig (optional): assumes current figure (gcf) if left unspecfied
+
+resolution_use = '-r600'; %'-r600' = 600 dpi
 
 if nargin < 2
     location = pwd;
 elseif nargin == 2
-    if strcmpi(location, 'russek')
-        location = 'C:\Users\kinsky.AD\Dropbox\Imaging Project\Presentations\Russek Day 2017\Poster';
-    elseif strcmpi(location, 'NO')
-        location = 'C:\Users\kinsky.AD\Dropbox\Imaging Project\Undergrads\Annalyse\plots';
-    elseif strcmpi(location, 'NOlaptop')
-        location = 'C:\Users\Nat\Dropbox\Imaging Project\Undergrads\Annalyse\plots';
+    switch location
+        case 'russek'
+            location = 'C:\Users\kinsky.AD\Dropbox\Imaging Project\Presentations\Russek Day 2017\Poster';
+        case '2env'
+            location = 'C:\Users\kinsky.AD\Dropbox\Imaging Project\Manuscripts\2env\Figures';
+        case 'NO'
+            location = 'C:\Users\kinsky.AD\Dropbox\Imaging Project\Undergrads\Annalyse\plots';
+        case 'NOlaptop'
+            location = 'C:\Users\Nat\Dropbox\Imaging Project\Undergrads\Annalyse\plots';
+        otherwise
     end
 end
 
+if nargin < 3
+    hfig = gcf;
+end
+
+% set to landscape or portrait
+if hfig.Position(3) > hfig.Position(4)
+    hfig.PaperOrientation = 'landscape';
+else
+    hfig.PaperOrientation = 'portrait';
+end
+
+hfig.Renderer = 'painters';
 save_file = fullfile(location, filename);
-print(save_file,'-dpdf','-bestfit')
+print(hfig, save_file,'-dpdf','-bestfit',resolution_use)
 
 end
 
