e8721c7d3dae48f6e788199c65fb88b6cb5b4045
nkinsky <kinsky@bu.edu>
2017-05-14 21:02:33 -0400

    bugfix

--- GCamp/helpers/plot_neuron_outlines.m
+++ GCamp/helpers/plot_neuron_outlines.m
@@ -3,10 +3,24 @@ function [ h , colors_used] = plot_neuron_outlines( base_image, NeuronImage, var
 %   plot outlines of neurons on top of the base image.  h is optional.  
 %   h (output) is axis handle to plot, and colors_used is
 %   a rgb array where each line is the color used to plot that neuron's
-%   outline
+%   outline.
+%
+%   INPUTS:
+%       base_image: Usually imread('ICmovie_min_proj.tif').  Set as NaN to
+%       skip plotting this part and either plot onto white or onto an
+%       existing plot
+%
+%       NeuronImage: from FinalOutpu.mat
+%
+%       h (optional): axes handle to plot into
+%
+%   OUTPUTS:
+%       h: axes handle to plot
+%
+%       colors_used: color table, good for using with plot_neuron_traces
 
 ip = inputParser;
-ip.addRequired('base_image',@isnumeric);
+ip.addRequired('base_image',@(a) isnumeric(a));
 ip.addRequired('NeuronImage',@iscell);
 ip.addOptional('h',nan,@ishandle);
 ip.parse(base_image, NeuronImage, varargin{:});
@@ -18,7 +32,9 @@ if ~ishandle(h)
 end
 
 axes(h)
-imagesc_gray(base_image); colorbar off; axis off
+if ~isnan(sum(base_image(:)))
+    imagesc_gray(base_image); colorbar off; axis off
+end
 hold on
 
 num_neurons = length(NeuronImage);
