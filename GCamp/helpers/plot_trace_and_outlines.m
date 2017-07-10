5bf25e7f8b58c2f9959916af45b3c3ec0ed7f2e6
Nat <kinsky@bu.edu>
2017-07-10 12:54:10 -0400

    enhancement - add in PSEs to trace plots

--- GCamp/helpers/plot_trace_and_outlines.m
+++ GCamp/helpers/plot_trace_and_outlines.m
@@ -1,15 +1,26 @@
-function [ ] = plot_trace_and_outlines( base_image, NeuronImage, NeuronTraces, neurons_to_plot, nframes_plot )
+function [ ] = plot_trace_and_outlines( base_image, NeuronImage, NeuronTraces, neurons_to_plot, nframes_plot, SR, hfig, PSAbool )
 % plot_trace_and_outlines( base_image, NeuronImage, NeuronTraces, neurons_to_plot, nframes_plot )
 % plots neuron outlines on top of base image with corresponding traces next
 % to it
 
-figure
+if nargin < 6
+    SR = 20;
+    if nargin < 7
+        hfig = figure;
+        if nargin < 8
+            PSAbool = false(size(NeuronTraces));
+        end
+    end
+    
+end
+
+figure(hfig);
 h1 = subplot(1,2,1);
 h2 = subplot(1,2,2);
 NeuronImage = NeuronImage(neurons_to_plot);
 traces = NeuronTraces(neurons_to_plot,1:nframes_plot);
 [~, colors] = plot_neuron_outlines(base_image, NeuronImage, h1);
-plot_neuron_traces(traces, colors, h2);
+plot_neuron_traces(traces, colors, h2, 'SR', SR, 'PSAbool', PSAbool);
 
 end
 
