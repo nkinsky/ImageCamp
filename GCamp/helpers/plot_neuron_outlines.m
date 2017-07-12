5a6c6167851a7ec96eacaa12fd8d2c3e47407376
nkinsky <kinsky@bu.edu>
2017-07-12 09:41:48 -0400

    enhancement

--- GCamp/helpers/plot_neuron_outlines.m
+++ GCamp/helpers/plot_neuron_outlines.m
@@ -1,5 +1,5 @@
 function [ h , colors_used] = plot_neuron_outlines( base_image, NeuronImage, varargin )
-% [h, colors_used]  = plot_neuron_outlines( base_image, NeuronImage,  h)
+% [h, colors_used]  = plot_neuron_outlines( base_image, NeuronImage,  h, varargin)
 %   plot outlines of neurons on top of the base image.  h is optional.  
 %   h (output) is axis handle to plot, and colors_used is
 %   a rgb array where each line is the color used to plot that neuron's
@@ -14,21 +14,37 @@ function [ h , colors_used] = plot_neuron_outlines( base_image, NeuronImage, var
 %
 %       h (optional): axes handle to plot into
 %
+%       colors (parameter): [r g b] color to use.  1 row = use for all the
+%       neurons, can also specify a different color for each row
+%
 %   OUTPUTS:
 %       h: axes handle to plot
 %
 %       colors_used: color table, good for using with plot_neuron_traces
 
+num_neurons = length(NeuronImage);
+
 ip = inputParser;
 ip.addRequired('base_image',@(a) isnumeric(a));
 ip.addRequired('NeuronImage',@iscell);
 ip.addOptional('h',nan,@ishandle);
+ip.addParameter('colors', nan, @(a) isnumeric(a) && size(a,2) == 3);
 ip.parse(base_image, NeuronImage, varargin{:});
 
 h = ip.Results.h;
+colors = ip.Results.colors;
 
 if ~ishandle(h)
     figure; h = gca;
+end
+
+if ~isnan(colors)
+   cust_col_flag = true; 
+   if size(colors,1) == 1
+       cust_colors = repmat(colors, num_neurons,1);
+   elseif size(colors,1) ~= num_neurons
+       error('number of custom colors specified does not match number of neurons')
+   end
 end
 
 axes(h)
@@ -37,12 +53,16 @@ if ~isnan(sum(base_image(:)))
 end
 hold on
 
-num_neurons = length(NeuronImage);
+
 colors_used = nan(num_neurons,3);
 
 for j = 1:num_neurons
     b = bwboundaries(NeuronImage{j},'noholes');
-    hline = plot(b{1}(:,2),b{1}(:,1));
+    if ~cust_col_flag
+        hline = plot(b{1}(:,2),b{1}(:,1));
+    elseif cust_col_flag
+        hline = plot(b{1}(:,2),b{1}(:,1),'Color',cust_colors(j,:));
+    end
     hline.LineWidth = 2;
     colors_used(j,:) = hline.Color;
 end
