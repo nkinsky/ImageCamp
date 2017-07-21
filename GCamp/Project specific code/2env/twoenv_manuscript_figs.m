50d89ba350c017f68528b87e0f30fabad226dac2
nkinsky <kinsky@bu.edu>
2017-07-21 17:37:08 -0400

    Figure 4 plots added in

--- GCamp/Project specific code/2env/twoenv_manuscript_figs.m
+++ GCamp/Project specific code/2env/twoenv_manuscript_figs.m
@@ -25,3 +25,26 @@ htraces = subplot(8,8,[5:8 (5:8)+8 (5:8)+16 (5:8)+24 (5:8)+32 (5:8)+40 (5:8)+48 
 plot_neuron_traces( NeuronTraces.(trace_plot)(neurons_plot,:), colors_used, htraces )
 hold off
 
+
+%% Figure 4 - connected occupancy maps with firing...
+mouse_use = 2;
+sesh_use = 6;
+corr_cutoff = 0.5;
+corr_mat_use = twoenv_squeeze(Mouse(mouse_use).corr_mat.circ2square);
+corrs_use = corr_mat_use{sesh_use,sesh_use};
+sesh_plot = Mouse(mouse_use).sesh.square(sesh_use);
+dir_use = ChangeDirectory_NK(sesh_plot);
+load(fullfile(dir_use,'Placefields_rot0.mat'),'PSAbool','x','y');
+num_neurons = size(PSAbool,1);
+figure(99); set(gcf,'Position',[2450 460 960 450]);
+n_out = 1; stay_in = true;
+neurons_use = find(corrs_use > corr_cutoff);
+while stay_in
+    neuron_plot = neurons_use(n_out);
+    plot(x,y,'k',x(PSAbool(neuron_plot,:)),y(PSAbool(neuron_plot,:)),'r*'); 
+    title(['Neuron ' num2str(neuron_plot) ' - ' mouse_name_title(sesh_plot.Animal) ' - ' ...
+        mouse_name_title(sesh_plot.Date) ' - ' num2str(sesh_plot.Session)])
+    axis off
+    [n_out, stay_in] = LR_cycle(n_out, [1 length(neurons_use)]);
+    
+end
