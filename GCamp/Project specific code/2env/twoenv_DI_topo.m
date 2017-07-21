956ddd909a8e7780d432e01654876a9fc7ca9c4e
nkinsky <kinsky@bu.edu>
2017-07-21 17:35:30 -0400

    roughly finished

--- GCamp/Project specific code/2env/twoenv_DI_topo.m
+++ GCamp/Project specific code/2env/twoenv_DI_topo.m
@@ -1,29 +1,80 @@
-function [ ] = twoenv_DI_topo( DI_use, batch_session_map, sesh_ind )
-% twoenv_DI_topo( DI_use, batch_map, sesh1_ind, sesh2_ind )
-%   Plot neuron ROIs with color coding for discrimination ratio
+function [ ] = twoenv_DI_topo( DI_use, batch_session_map, sesh_ind, varargin )
+% twoenv_DI_topo( DI_use, batch_map, sesh1_ind, sesh2_ind, varargin )
+%   Plot neuron ROIs with color coding for discrimination ratio.
 
+high_DI_thresh = 0.75;
+low_DI_thresh = 0.25;
+
+%% ID valid neurons and classify as high DI or low DI
 % ID valid neurons
-valid_neurons = ~isnan(DI_use);
+valid_neurons = find(~isnan(DI_use));
+
 DI_valid = DI_use(valid_neurons);
+
+% Identify high DI (Discriminator) neurons and low DI neurons (could later
+% be classified as coherent or not...)
+high_DI = abs(DI_valid) <= 1 & abs(DI_valid) > high_DI_thresh;
+low_DI = abs(DI_valid) < low_DI_thresh;
+neither = abs(DI_valid) >= low_DI_thresh & abs(DI_valid) <= high_DI_thresh;
+
+%% Dump everything into sesh variable
 for j = 1:2
-    sesh(1).neurons = batch_session_map.map(valid_neurons, sesh_ind(j)+1);
+    % neurons active in either session
+    sesh(j).neurons = batch_session_map.map(valid_neurons, sesh_ind(j)+1);
+    
+    % high DI and low DI neurons in each session, and those that are
+    % neither
+    sesh(j).high_DI = batch_session_map.map(valid_neurons(high_DI), sesh_ind(j)+1);
+    sesh(j).low_DI = batch_session_map.map(valid_neurons(low_DI), sesh_ind(j)+1);
+    
+    % Load in ROIs for each session
     sesh_dir = ChangeDirectory_NK(batch_session_map.session(sesh_ind(j)),0);
     load(fullfile(sesh_dir,'FinalOutput.mat'),'NeuronImage');
-    sesh(j).ROIs = NeuronImage;
+    if j == 1
+        sesh(j).ROIs = NeuronImage;
+    elseif j == 2
+        sesh(j).ROIs = ROIwarp(batch_session_map.session(sesh_ind(1)),...
+            batch_session_map.session(sesh_ind(2)));
+    end
+    %%% Need to register 2nd session to 1st before plotting
 end
 
-keyboard
+% neurons active in both sessions
+% active_both_log = (sesh(1).neurons ~= 0 & sesh(2).neurons ~= 0);
 
-high_DI = abs(DI_valid) <= 1 & abs(DI_valid) > 0.75;
-low_DI = abs(DI_valid) < 0.25;
+%% Plot everything
+figure; h = gca;
 
-h = figure;
+% Plot all neuron outlines
 for j=1:2
-    
-    plot_neuron_outlines(nan, NeuronImage, h, 'colors', [0 0 1]); % Plot all neurons
+    [~, ~, h_neither] = plot_neuron_outlines(nan, sesh(j).ROIs, h, 'colors', [0 0 1]); % Plot all neurons
 end
 
+% Plot high DI in red, low DI in green for each session
+for j=1:2
+    [~, ~, h_high] = plot_neuron_outlines(nan, sesh(j).ROIs(sesh(j).high_DI(sesh(j).high_DI ~= 0)), ...
+        h, 'colors', [1 0 0]); % Plot high DI red
+    [~, ~, h_low] = plot_neuron_outlines(nan, sesh(j).ROIs(sesh(j).low_DI(sesh(j).low_DI ~= 0)), ...
+        h, 'colors', [0 1 0]); % Plot low DI green
+end
 
+legend(cat(2,h_high,h_low,h_neither),{'High DI','Low DI','tweener'})
+
+%% Potentially a bit cleaner looking plots, but not validated yet
+% 
+% plot_neuron_outlines(nan, sesh(1).ROIs, h, 'colors', [0 0 1]); % Plot all neurons
+% plot_neuron_outlines(nan, sesh(2).ROIs(valid_neurons(~active_both_log)), ...
+%     h, 'colors', [0 0 1]); % Plot all neurons
+% 
+% plot_neuron_outlines(nan, sesh(1).ROIs(sesh(1).high_DI(sesh(j).high_DI ~= 0)), ...
+%     h, 'colors', [1 0 0]); % Plot high DI red
+% plot_neuron_outlines(nan, sesh(1).ROIs(sesh(j).low_DI(sesh(j).low_DI ~= 0)),...
+%     h, 'colors', [0 1 0]); % Plot low DI green
+% 
+% plot_neuron_outlines(nan, sesh(2).ROIs(sesh(1).high_DI(sesh(j).high_DI ~= 0)), ...
+%     h, 'colors', [1 0 0]); % Plot high DI red
+% plot_neuron_outlines(nan, sesh(2).ROIs(sesh(j).low_DI(sesh(j).low_DI ~= 0)),...
+%     h, 'colors', [0 1 0]); % Plot low DI green
 
 end
 
