57028b61ec6ce8699a172c992c8283f6e2ff58ee
nkinsky <kinsky@bu.edu>
2017-10-31 08:26:58 -0400

    enhancement

--- GCamp/assign_FR.m
+++ GCamp/assign_FR.m
@@ -1,12 +1,17 @@
-function [ FR_vector_master] = assign_FR( FR_vector_session, map_use )
-% FR_vector_master = assign_FR( FR_vector_session, map_use )
+function [ FR_vector_master] = assign_FR( FR_vector_session, map_use, silent_include )
+% FR_vector_master = assign_FR( FR_vector_session, map_use, zero_FR )
 %
 % Takes a vector of firing rates from a given session (FR_vector_session) and the neuron
 % mapping to the master session and outputs the same vector but with each
 % row corresponding to the master neuron number (FR_vector_master).  Puts nans where map_use
-% indicates a bad mapping. FR_vectors are n x 1 vectors
+% indicates a bad mapping. FR_vectors are n x 1 vectors.
+%
+%   silent_include: true = set silent cell rates (batch_map value = 0) to 0
+%   (default).  false = set to NaN.
 
-
+if nargin < 3
+    silent_include = true;
+end
 
 % map_use = batch_map(:,j+1);
 %%
@@ -15,7 +20,11 @@ nan_neurons = isnan(map_use);
 temp = unique(map_use); % Get numbers of all neurons properly mapped in this session
 map_unique = temp(~isnan(temp) & temp ~= 0); 
 
-temp_FR = zeros(size(map_use,1),1);
+if silent_include
+    temp_FR = zeros(size(map_use,1),1);
+elseif ~silent_include
+    temp_FR = nan(size(map_use,1),1);
+end
 %%
 for k = 1:length(map_unique) % 1:max(map_use)
     neuron_id = map_use == map_unique(k);
