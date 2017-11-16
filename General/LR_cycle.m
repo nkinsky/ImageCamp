9c447c7c141500a55d7de676fdb57436cd3b0f4b
Nat Kinsky <kinsky@bu.edu>
2017-11-16 13:58:23 -0500

    Enhancements

--- General/LR_cycle.m
+++ General/LR_cycle.m
@@ -1,12 +1,14 @@
-function [ n_out, stay_in] = LR_cycle( n_in, n_range)
-% [ n_out, stay_in] = LR_cycle( n_in, n_range)
+function [ n_out, stay_in, x, y, button] = LR_cycle( n_in, n_range)
+% [ n_out, stay_in, x, y, button] = LR_cycle( n_in, n_range)
 %
 %   Lets you cycle n_in up one value or down one value depending on if you
 %   push the left or right buttons (spacebar exits).  Similar use to
 %   waitforbuttonpress within a for loop, except using LR_cycle in a while
 %   loop allows you to step forward or backward through plots.  n_range is 
 %   [n_min, n_max], the range of ns you want to scroll through. escape_flag
-%   is whatever you want to use to exit your while loop.
+%   is whatever you want to use to exit your while loop. x, y, button gives you
+%   the button you pushed and where if you want it. To scroll through 10 at
+%   a time use 7 and 9 in liue of L and R.
 %
 %   NOTE: typing 'k' allows you to enter keyboard mode (i.e. to print or
 %   save the figure). Type 'dbcont' to re-enter scroll mode.
@@ -23,7 +25,8 @@ function [ n_out, stay_in] = LR_cycle( n_in, n_range)
 %   end
 
 % ASCII definitions here
-right = 29; left = 28; spacebar = 32; k = 107;
+right = 29; left = 28; spacebar = 32; k = 107; rightmouse = 3;
+right9 = 57; left7 = 55;
 
 %% Set up and display output
 persistent disp_flag; % Set persistent variable so that you only display 
@@ -36,32 +39,40 @@ end
 
 %% Run it
 try
-    [~,~,button] = ginput(1);
+    [x,y,button] = ginput(1);
     switch button
-        case left
-            n_out = n_in - 1;
+        case {left, left7}
+            if button == left
+                n_out = n_in - 1;
+            elseif button == left7
+                n_out = n_in - 10;
+            end
             stay_in = true;
             
             % Skip to end if you are at beginning and hit left
             if n_out < min(n_range)
                 n_out = max(n_range);
             end
-        case right
-            n_out = n_in + 1;
+        case {right, right9}
+            if button == right
+                n_out = n_in + 1;
+            elseif button == right9
+                n_out = n_in + 10;
+            end
             stay_in = true;
             
             % Skip to beginning if you are at end and hit right
             if n_out > max(n_range)
                 n_out = min(n_range);
             end
-        case spacebar
-            n_out = [];
+        case {spacebar, rightmouse}
+%             n_out = [];
+            n_out = n_in;
             stay_in = false;
             clear disp_flag
         case k
             disp('Stepping into LR_cycle.  Type dbcont to keep on running')
             
-
             keyboard
             n_out = n_in;
             stay_in = true;
