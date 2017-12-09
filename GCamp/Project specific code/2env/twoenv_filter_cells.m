72a24b8ec1a2d786594c0f68df86a27654d27173
nkinsky <kinsky@bu.edu>
2017-12-08 21:49:50 -0500

    bugfix

--- GCamp/Project specific code/2env/twoenv_filter_cells.m
+++ GCamp/Project specific code/2env/twoenv_filter_cells.m
@@ -1,12 +1,13 @@
 function [ coh_bool, remap_bool, silent_bool, valid_bool ] = twoenv_filter_cells( ...
-    best_angle_pop, best_angle_all, PSAbool1, PSAbool2, map_use)
+    best_angle_pop, best_angle, PSAbool1, PSAbool2, map_use)
 % [ coh_bool, remap_bool, silent_bool, valid_bool ] = twoenv_filter_cells( ...
 %     best_angle_pop, best_angle_all, PSAbool1, PSAbool2, map_use)
 %
 %   Filters cells into coherent, remapping, and silent cells for later use.
-% INPUTS: best_angle_pop and best_angle_all are from full_rotation_analysis
-% saved files, PSAbool1/2 are from the two sessions in question, map_use is
-% the map between sessions (get_neuronmap_from_batchmap).
+%   
+%   INPUTS: best_angle_pop and best_angle_all are from full_rotation_analysis
+%   saved files, PSAbool1/2 are from the two sessions in question, map_use is
+%   the map between sessions (get_neuronmap_from_batchmap).
 
 % 1) get valid cells - those with good mapping between session 1 and session 2
 valid_bool = ~isnan(map_use) & map_use ~= 0;
@@ -20,7 +21,7 @@ act2_ref(valid_bool) = act2(map_use(valid_bool));
 silent_bool = (act1 ~= 0 & act2_ref == 0) | (act1 == 0 & act2_ref ~= 0);
 
 % 3) Get coherent cells
-coh_bool_temp = get_coherent_neurons(best_angle_pop,best_angle);
+coh_bool_temp = get_coherent_neurons(best_angle_pop, best_angle);
 coh_bool = coh_bool_temp & ~silent_bool;
 
 % 4) Get remapping cells
