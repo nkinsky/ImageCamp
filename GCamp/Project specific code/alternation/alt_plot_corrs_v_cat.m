c2ec7f426accf017a1751a348ed33dd246983b92
nkinsky <kinsky@bu.edu>
2017-12-09 23:11:17 -0500

    Cleaned up and debugged

--- GCamp/Project specific code/alternation/alt_plot_corrs_v_cat.m
+++ GCamp/Project specific code/alternation/alt_plot_corrs_v_cat.m
@@ -23,7 +23,7 @@ ntrans_thresh = ip.Results.ntrans_thresh;
 
 %% Step 1: register sessions
 % Get map and cells the go silent or become active
-[neuron_map, become_silent, become_active] = neuron_map_simple(MDbase, MDreg);
+neuron_map = neuron_map_simple(MDbase, MDreg, 'suppress_output', true);
 coactive_bool = ~isnan(neuron_map) & neuron_map ~= 0;
 
 %% Step 2: load tmaps for each & register
@@ -36,23 +36,8 @@ rhos = cellfun(@(a,b) corr(a(:),b(:),'type','Spearman','rows','complete'),...
 %% Step 4: Parse cells into splitters (1), stem PCs(2), stem NPCs (3), 
 % arm PCs(4) ,and arm NPCs(5), 0 = doesn't pass ntrans threshold
 
-ntrans_pass = cell(1,2);
-categories = cell(1,2);
-for j = 1:2
-   load(fullfile(sesh(j).Location,'sigSplitters.mat'),'neuronID','sigcurve');
-   stem_cells = false(length(sigcurve),1);
-   categories{j} = zeros(length(sigcurve),1);
-   stem_cells(neuronID) = true;
-   [pctemp, ntrans_pass] = pf_filter(sesh(j), pval_thresh, ntrans_thresh, ...
-       PFname);
-   categories{j}(stem_cells & cellfun(@any,sigcurve) & ntrans_pass) = 1;
-   categories{j}(stem_cells & pctemp & ~cellfun(@any,sigcurve) & ...
-       ntrans_pass) = 2;
-   categories{j}(stem_cells & ~pctemp & ~cellfun(@any,sigcurve) & ...
-       ntrans_pass) = 3;
-   categories{j}(~stem_cells & pctemp & ntrans_pass) = 4;
-   categories{j}(~stem_cells & ~pctemp & ntrans_pass) = 5;
-end
+categories = arrayfun(@(a) alt_parse_cell_category(a, pval_thresh, ...
+    ntrans_thresh, PFname), sesh, 'UniformOutput', false);
 
 % Dump these into an array for all the validly mapped cells between each
 % session
@@ -63,26 +48,15 @@ category_array(:,2) = categories{2}(neuron_map(coactive_bool));
 cat_names = {'Splitters','Stem PCs', 'Stem NPCs', 'Arm PCs', 'Arm NPCs'};
 position = [230 360 780 430];
 good_cells = category_array(:,1) ~= 0;
-scatterBox(rhos(good_cells), category_array(good_cells,1),'xLabels',...
+ha = scatterBox(rhos(good_cells), category_array(good_cells,1),'xLabels',...
     cat_names, 'yLabel', '\rho (Spearman)','position',position,...
     'transparency', 0.6);
+title({[mouse_name_title(MDbase.Animal) ': ' num2str(get_time_bw_sessions(MDbase, MDreg)) ... 
+    ' day lag'], [mouse_name_title(MDbase.Date) 's' ...
+    num2str(MDbase.Session) ' to ' mouse_name_title(MDreg.Date) 's' ...
+    num2str(MDreg.Session)]})
 
-%% Step 6: Now get proportions that stay in each category
-for j= 1:5
-    stay_by_cat(j) = sum(category_array(:,1) == category_array(:,2) ...
-        & category_array(:,2) == j);
-    stay_prop(j) = stay_by_cat(j)/sum((category_array(:,1) == j));
-end
-
-%% Step 7: Now step through and get proportions that are coactive in each category
-for j = 1:5
-    coactive_by_cat(j) = sum(coactive_bool & categories{1} == j);
-    coactive_prop(j) = coactive_by_cat(j)/sum(categories{1} == j);
-end
-
-%%% NRK Note - to avoid creating your normal mega functions, break up
-%%% anything above into functions and then create a second function to do
-%%% steps 6/7
+%% Step 6: do an ANOVA on all the categories!!!
 
 end
 
