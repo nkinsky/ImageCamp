95adc97f54f724caf7a29f0f1c6ca3e37f56a5d3
nkinsky <kinsky@bu.edu>
2017-12-09 23:13:01 -0500

    enhancement + comment on needed future minor bugfix and try/catch to avoid it for now

--- GCamp/reg_qc_plot_batch.m
+++ GCamp/reg_qc_plot_batch.m
@@ -1,4 +1,4 @@
-function [reg_stats] = reg_qc_plot_batch(base, reg, varargin)
+function [reg_stats, hfig] = reg_qc_plot_batch(base, reg, varargin)
 % reg_stats = reg_qc_plot_batch(base, reg, num_shuffles (opt), num_shifts(opt), shift_dist(opt), ...)
 % Default is 100 shuffles, 10 shifts, and 4 pixel shift
 %%% NRK 1) need to update this for including batch map with ALL masks
@@ -12,11 +12,13 @@ function [reg_stats] = reg_qc_plot_batch(base, reg, varargin)
 p = inputParser;
 p.addRequired('base', @isstruct);
 p.addRequired('reg', @isstruct);
-p.addOptional('num_shuffles', 100, @(a) isnumeric(a) && round(a) == a && a > 0);
+p.addOptional('num_shuffles', 100, @(a) isnumeric(a) && round(a) == a && a >= 0);
 p.addOptional('num_shifts', 0, @(a) isnumeric(a) && round(a) == a && a >= 0);
 p.addOptional('shift_dist', 4, @(a) isnumeric(a) && a > 0);
 p.addParameter('batch_mode',0, @(a) a == 0 || a == 1 || a == 2);
-p.addParameter('name_append','',@(a) ischar(a) || iscell(a) && (length(a) == 1 || length(a) == length(reg)));
+p.addParameter('name_append','',@(a) ischar(a) || iscell(a) && (length(a) == 1 ...
+    || length(a) == length(reg)));
+p.addParameter('hfig', [], @ishandle);
 p.parse(base, reg, varargin{:});
 
 num_shuffles = p.Results.num_shuffles;
@@ -24,6 +26,7 @@ num_shifts = p.Results.num_shifts;
 shift_dist = p.Results.shift_dist;
 batch_mode = p.Results.batch_mode;
 name_append = p.Results.name_append;
+hfig = p.Results.hfig;
 
 if length(reg) > 1
     multi_sesh = 1;
@@ -40,7 +43,9 @@ end
 
 %% Plot
 
-h = figure;
+if isempty(hfig)
+    hfig = figure;
+end
 
 reg_stats = cell(length(reg),1);
 legend_text = cell(1, length(reg));
@@ -50,14 +55,14 @@ reg_stats{1} = neuron_reg_qc(base, reg(1), 'batch_mode', batch_mode, ...
     'name_append', name_append{1}); % If the last registration is bad you can get a bad shuffled distribution here...
 legend_text{1} = [mouse_name_title(reg(1).Date) ' - #' num2str(reg(1).Session)];
 reg_qc_plot(reg_stats{1}.cent_d, reg_stats{1}.orient_diff, ...
-        reg_stats{1}.avg_corr, h, 'multi_sesh', multi_sesh);
+        reg_stats{1}.avg_corr, hfig, 'multi_sesh', multi_sesh);
 
 for j = 2:length(reg)
     
     reg_stats{j} = neuron_reg_qc(base, reg(j), 'batch_mode', batch_mode,...
         'name_append', name_append{j});
     reg_qc_plot(reg_stats{j}.cent_d, reg_stats{j}.orient_diff, ...
-        reg_stats{j}.avg_corr, h, 'multi_sesh', 1);
+        reg_stats{j}.avg_corr, hfig, 'multi_sesh', 1);
     legend_text{j} = [mouse_name_title(reg(j).Date) ' - #' num2str(reg(j).Session)];
     reg_stats{j}.session = reg(j);
 end
@@ -70,8 +75,8 @@ end
 %         reg_stats{end}.avg_corr, h, 'multi_sesh', multi_sesh);
     
 reg_qc_plot(reg_stats{1}.shift.cent_d, reg_stats{1}.shift.orient_diff, ...
-    reg_stats{1}.shift.avg_corr, h, 'plot_shuf', 1);
-reg_qc_plot([], reg_stats{1}.shuffle.orient_diff, [], h, 'plot_shuf', 1);
+    reg_stats{1}.shift.avg_corr, hfig, 'plot_shuf', 1);
+reg_qc_plot([], reg_stats{1}.shuffle.orient_diff, [], hfig, 'plot_shuf', 1);
 
 for j=1:4
     subplot(2,2,j)
@@ -89,10 +94,16 @@ if num_shuffles > 0
     legend(updatelegend(gca,'Shuffled'));
 end
 
+subplot(2,2,4)
+title(['Base Session: ' mouse_name_title(base.Animal) ' - ' ...
+    mouse_name_title(base.Date) ' session ' num2str(base.Session)])
+
 %% Set colors from cool (nearest in time) to warm (furthest in time)
+% NRK - this does NOT work for pair-wise registration, so added try/catch for now 
 ff = get(gcf);
 num_lines = length(ff.Children(8).Children); % Get number of lines drawn
 cm = flipud(colormap(jet(num_lines)));
+try
 if num_shuffles > 0
     for j = 2:2:4
         cm_use = [0 0 0; cm];
@@ -107,12 +118,21 @@ else
         end
     end
 end
+catch
+    disp('bypassing colors for lines in reg_qc_plot_batch for now')
+end
 
+try
 for j = 6:2:8
     for k = 1: size(cm,1)
         ff.Children(j).Children(k).Color = cm(k,:);
     end
 end
+catch
+    disp('bypassing colors for lines in reg_qc_plot_batch for now')
+end
+
+
 
 end
 
