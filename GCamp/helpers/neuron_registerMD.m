937fcb155fd66a75d99e812b13be6f421b572b78
nkinsky <kinsky@bu.edu>
2017-12-11 09:07:03 -0500

    nada

--- GCamp/helpers/neuron_registerMD.m
+++ GCamp/helpers/neuron_registerMD.m
@@ -3,8 +3,7 @@ function [ neuron_map ] = neuron_registerMD( MDbase, MDreg, varargin )
 %   registers MDreg to MD base usin neuron_register
 
 neuron_map = neuron_register(MDbase.Animal, MDbase.Date, MDbase.Session,...
-    MDreg.Date, MDreg.Session,varargin{:});
-
+    MDreg.Date, MDreg.Session, varargin{:});
 
 end
 
