91d9b6651cc7467bc4f6fd3c1622d4fa8600ac35
nkinsky <kinsky@bu.edu>
2018-01-11 10:38:15 -0500

    bugfix

--- GCamp/Project specific code/2env/twoenv_filter_cells.m
+++ GCamp/Project specific code/2env/twoenv_filter_cells.m
@@ -17,16 +17,20 @@ act1 = sum(PSAbool1,2)/size(PSAbool1,2); %Get mean activity of each neuron in se
 act2 = sum(PSAbool2,2)/size(PSAbool2,2);
 act2_ref = nan(length(act1),1);
 act2_ref(valid_bool) = act2(map_use(valid_bool));
+
 % Get cells active in one session but not the other
-silent_bool = (act1 ~= 0 & act2_ref == 0) | (act1 == 0 & act2_ref ~= 0);
+silent_bool = (act1 ~= 0 & act2_ref == 0) | (act1 == 0 & act2_ref ~= 0 & ...
+    ~isnan(act2_ref));
+silent_bool(~valid_bool) = false;
 
 % 3) Get coherent cells
 coh_bool_temp = get_coherent_neurons(best_angle_pop, best_angle);
 coh_bool = coh_bool_temp & ~silent_bool;
+coh_bool(~valid_bool) = false;
 
 % 4) Get remapping cells
 remap_bool = ~coh_bool_temp & ~silent_bool;
-
+remap_bool(~valid_bool) = false;
 
 end
 
--- GCamp/Project specific code/2env/twoenv_filter_cells2.m
+++ GCamp/Project specific code/2env/twoenv_filter_cells2.m
@@ -15,7 +15,7 @@ if strcmpi(comp_type,'circ2square')
     ind_map = [1 2 1 2 3 4 3 4 5 5 6 6 7 8 7 8];
     type_map = [1 1 2 2 2 2 1 1 1 2 2 1 1 1 2 2]; % 1 = sq, 2 = circ
     load(fullfile(base_dir,'batch_session_map_trans.mat'));
-    load(fullfile(base_dir,'full_rotation_analysis_circ2square_shuffle1000.mat'),...
+    load(fullfile(base_dir,'full_rotation_analysis_circ2square_TMap_gauss_shuffle1000.mat'),...
         'best_angle','best_angle_all');
     pf_file = 'Placefields_cm4_trans_rot0.mat';
 else 
@@ -23,24 +23,35 @@ else
     load(fullfile(base_dir,'batch_session_map.mat'));
     pf_file = 'Placefields_cm4_rot0.mat';
     if strcmpi(comp_type,'circle')
-        load(fullfile(base_dir,'full_rotation_analysis_circle_shuffle1000.mat'),...
+        load(fullfile(base_dir,'full_rotation_analysis_circle_TMap_gauss_shuffle1000.mat'),...
             'best_angle','best_angle_all');
     elseif strcmpi(comp_type,'square')
-        load(fullfile(base_dir,'full_rotation_analysis_square_shuffle1000.mat'),...
+        load(fullfile(base_dir,'full_rotation_analysis_square_TMap_gauss_shuffle1000.mat'),...
             'best_angle','best_angle_all');
     end
 end
-
-sesh1_ind = get_session_index(sesh1,batch_session_map.session);
-sesh2_ind = get_session_index(sesh2,batch_session_map.session);
-best_angle_pop = best_angle(ind_map(sesh1_ind), ind_map(sesh2_ind));
-best_angle_dist = best_angle_all{ind_map(sesh1_ind), ind_map(sesh2_ind)};
+batch_session_map = fix_batch_session_map(batch_session_map);
 
 load(fullfile(sesh(1).dir,pf_file),'PSAbool','xBin','yBin');
 PSAbool1 = PSAbool(:,(xBin ~= 0 & yBin ~= 0));
 
 load(fullfile(sesh(2).dir,pf_file),'PSAbool','xBin','yBin');
 PSAbool2 = PSAbool(:,(xBin ~= 0 & yBin ~= 0));
+
+sesh1_ind = get_session_index(sesh1,batch_session_map.session);
+sesh2_ind = get_session_index(sesh2,batch_session_map.session);
+if sesh1_ind == sesh2_ind
+    best_angle_pop = 0;
+    best_angle_dist = zeros(size(PSAbool,1),1);
+elseif sesh1_ind ~= sesh2_ind
+    best_angle_pop = best_angle(ind_map(sesh1_ind), ind_map(sesh2_ind));
+    best_angle_dist = best_angle_all{ind_map(sesh1_ind), ind_map(sesh2_ind)};
+    if isnan(best_angle_pop)
+        best_angle_pop = best_angle(ind_map(sesh2_ind), ind_map(sesh1_ind));
+        best_angle_dist = best_angle_all{ind_map(sesh2_ind), ind_map(sesh1_ind)};
+    end
+        
+end
 
 map_use = get_neuronmap_from_batchmap(batch_session_map, sesh1_ind, sesh2_ind);
 
