c5987049d45e5045833f5d83e093c46bbc978c39
nkinsky <kinsky@bu.edu>
2018-07-25 10:03:32 -0400

    enhancement

--- GCamp/Project specific code/alternation/sigtuning_batch.m
+++ GCamp/Project specific code/alternation/sigtuning_batch.m
@@ -1,27 +1,37 @@
-perf = [];
+function [perf] = sigtuning_batch(sesh_sigtuning, PFname)
+% perf = sigtuning_batch(sesh_sigtuning, PFname)
+%   Run sigtuning on all sessions in sesh_sigtuning. Spits out performance
+%   data including hand scored performance, automated performance, percent
+%   splitters and total number of splitters. Loads variables from PFname
+%   (e.g. Placefields_1cm.mat)
+
 for j = 1:length(sesh_sigtuning)
     skip_perf = 0;
     ChangeDirectory_NK(sesh_sigtuning(j))
     if exist('sigSplitters.mat','file') ~= 2
-        try
-            load('PlaceMaps.mat','x','y','FT');
+%         try
+            load(PFname,'x','y','PSAbool');
             disp(['Running sigtuningAllCells for session ' num2str(sesh_sigtuning(j).Session) ...
                 ' from ' sesh_sigtuning(j).Date])
-            sigtuningAllCells(x,y,FT);
-        catch
-            disp('Required files missing - skipping for now')
-            skip_perf = 1;
-        end
+            sigtuningAllCells(x,y,PSAbool);
+%         catch
+%             disp('Required files missing - skipping for now')
+%             skip_perf = 1;
+%         end
     elseif exist('sigSplitters.mat','file') == 2
         disp(['sigTuningAllCells already run for session ' num2str(sesh_sigtuning(j).Session) ...
         ' from ' sesh_sigtuning(j).Date])
     end
     perf(j).Date = sesh_sigtuning(j).Date;
+    
     if skip_perf == 0
         load('Alternation.mat');
         perf(j).performance = sum(Alt.summary(:,3))/size(Alt.summary,1);
         load sigSplitters.mat
         perf(j).numSplitters = numSplitters;
         perf(j).percentSplitters = numSplitters/length(neuronID);
+        perf(j).hand_perf = sesh_sigtuning(j).perf;
     end
+end
+
 end
--- GCamp/helpers/T4batch.m
+++ GCamp/helpers/T4batch.m
@@ -1,16 +1,23 @@
 function [success_bool] = T4batch(sesh)
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
+% success_bool = T4batch(sesh)
+% Run Tenaspis4 in batch mode. Spit out a boolean letting you know if each
+% session ran properly or not.
 
 nsesh = length(sesh);
 success_bool = false(1,nsesh);
 for j = 1:nsesh
-   try
-       Tenaspis4(sesh(j));
-       success_bool(j) = true;
-   catch
-       disp(['Error running session ' num2str(j)])
-   end
+    if exist(fullfile(sesh(j).Location,'FinalOutput.mat'),'file')
+        disp(['FinalOutput.mat found for ' mouse_name_title(sesh(j).Date) ...
+            ' - session ' num2str(sesh(j).Session)])
+        success_bool(j) = true;
+        continue
+    end
+    try
+        Tenaspis4(sesh(j));
+        success_bool(j) = true;
+    catch
+        disp(['Error running session ' num2str(j)])
+    end
 end
 
 end
