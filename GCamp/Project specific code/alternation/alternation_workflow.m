ba42c04bb8bea3534e2bf8c51b4eabe994e7b92f
nkinsky <kinsky@bu.edu>
2018-07-31 09:06:30 -0400

    lots of stuff

--- GCamp/Project specific code/alternation/alternation_workflow.m
+++ GCamp/Project specific code/alternation/alternation_workflow.m
@@ -10,7 +10,7 @@ save('Pos.mat','n_image_frames','x_use','y_use','t_use','MoMtime','start_time',.
     'xpos_interp','ypos_interp','time_interp','exclude_time_interp')
 
 %% 0.5) First plot everything and figure out what needs manual limits
-base_sesh = G30_alt(1); % base session
+base_sesh = G31_alt(1); % base session
 % todo_sesh = MD([203 204 206 212 213]);
 todo_sesh = run_pos_align; % sessions to do
 % todo_sesh = MD(213);
@@ -30,11 +30,11 @@ for j = 1:length(todo_sesh)
     exclude_frames = false(size(time_interp)) ; exc_time_min = nan; exc_time_max = nan;
     % Comment out below to check before doing pos_align!
     if exist('exclude_time_interp','var')
-        % Below would work but there was a bug in Sam's PreProcess and now
-        % the time in between in too small. Use for G48 10/1 sesh2.
+% %         Below would work but there was a bug in Sam's PreProcess and now
+% %         the time in between in too small. Use for G48 10/1 sesh2.
 %                exclude_frames = (time_interp >= min(exclude_time_interp)) & ...
 %                    (time_interp <= max(exclude_time_interp));
-        
+%         
         exc_time_min = min(exclude_time_interp);
         load(fullfile(todo_sesh(j).Location,'part2','Pos.mat'),...
             'MouseOnMazeFrame','start_time');
@@ -74,9 +74,10 @@ batch_align_pos(base_sesh,todo_sesh,'base_adjust',false,...
 %% 2) Run Placefields_batch on all new sessions - make sure to use
 % exclude_time_interp!!! Note this only works for sets of 2 sessions - if
 % there are more than 2 sessions combined you will need to revisit this!
+todo_sesh = run_pf;
 
 exc_times = []; suc_bool = false(1,length(todo_sesh));
-for j = 10:length(todo_sesh)
+for j = 1:length(todo_sesh)
     try
         sesh_use = todo_sesh(j);
         
@@ -87,10 +88,10 @@ for j = 10:length(todo_sesh)
         load(fullfile(sesh_use.Location,'Pos.mat'),'exclude_time_interp')
         exclude_frames = []; exc_time_min = nan; exc_time_max = nan;
         if exist('exclude_time_interp','var')
-            % Below would work but there was a bug in Sam's PreProcess and now
-            % the time in between in too small.
-            %        exclude_frames = find((time_interp >= min(exclude_time_interp)) & ...
-            %            (time_interp <= max(exclude_time_interp)));
+%             % Below would work but there was a bug in Sam's PreProcess and now
+%             % the time in between in too small.
+%                    exclude_frames = find((time_interp >= min(exclude_time_interp)) & ...
+%                        (time_interp <= max(exclude_time_interp)));
             
             exc_time_min = min(exclude_time_interp);
             load(fullfile(sesh_use.Location,'part2','Pos.mat'),...
@@ -98,6 +99,7 @@ for j = 10:length(todo_sesh)
             exc_time_max = exc_time_min + MouseOnMazeFrame/30+start_time;
             exclude_frames = find((time_interp >= exc_time_min) & ...
                 (time_interp <= exc_time_max));
+%             keyboard
             
         end
         exc_times = [exc_times; exc_time_min exc_time_max];
@@ -113,15 +115,35 @@ end
 
 %% 2.5) Run PF stats too - not sure if I truly need this yet, but might as 
 % well run it now anyway
+todo_sesh = G48_alt;
 
 for j = 1:length(todo_sesh)
     sesh_use = todo_sesh(j);
-    PlacefieldStats(sesh_use,'name_append','_cm1');
+    PFstats_name = fullfile(ChangeDirectory_NK(sesh_use,0),'PlacefieldStats_cm1.mat');
+    if ~exist(PFstats_name,'file')
+        PlacefieldStats(sesh_use,'name_append','_cm1');
+    else
+        disp([' ''PlacefieldStats_cm1.mat'' found for session ' num2str(j)])
+    end
 end
 
-%% 3) Run sigtuning_batch on all new sessions
-sesh_use = [];
-perf = sigtuning_batch(sesh_use, 'Placefields_cm1.mat');
+%% 2.8) ID transition from forced to free and run Placefields half in
+% relevant sessions
+
+%% 2.9) Run PF on each half of above sessions, save forced as "..._forced_cm1"
+% and combines as "..._combined_cm1..." and free normally "..._cm1"
+
+
+%% 3) Run sigtuning_batch on all new sessions - note that this uses Placefields
+% that have been made using a 1 cm/s speed threshold and excludes all times
+% the mouse is below this speed. Should be ok except for times when the
+% mouse is just putzing around the whole time!
+% sesh_use = G30_alt;
+% sesh_use = complete_MD(sesh_use);
+% perf = sigtuning_batch(sesh_use, 'Placefields_cm1.mat');
+G31_alt = sigtuning_batch(complete_MD(G31_alt), 'Placefields_cm1.mat');
+G45_alt = sigtuning_batch(complete_MD(G45_alt), 'Placefields_cm1.mat');
+G48_alt = sigtuning_batch(complete_MD(G48_alt), 'Placefields_cm1.mat');
 
 %% 4) Run plotSigSplitters in batch mode (code located in
 % alternation_scratchpad) for all new sessions
