a8ded98e5c4942612dbfb701b1118188c7df4291
nkinsky <kinsky@bu.edu>
2018-08-31 13:29:47 -0400

    now utilizes hand check of registrations

--- GCamp/Project specific code/alternation/alt_stab_v_cat_batch.m
+++ GCamp/Project specific code/alternation/alt_stab_v_cat_batch.m
@@ -1,5 +1,5 @@
 function [ stay_prop_all, coactive_prop_all, cat_names ] = ...
-    alt_stab_v_cat_batch(day_lag, comp_type, mice_sesh )
+    alt_stab_v_cat_batch(day_lag, comp_type, mice_sesh, PFname )
 %  [stay_prop, coactive_prop, cat_names ] = alt_stab_v_cat_batch(...
 %       day_lag, comp_type, mouse1, mouse2,... )
 %   Batch function for getting splitter stability for all mice at the
@@ -10,6 +10,10 @@ function [ stay_prop_all, coactive_prop_all, cat_names ] = ...
 %   containing data structure for each mouse for combined plotting
 %   color_mice parameter set to true (default) colors each mouse's data
 %   point separately, false plots all in black
+
+if nargin < 4
+    PFname = 'Placefields.mat';
+end
 
 % Deal out mice_sesh into appropriate variable (must be a cell)
 if iscell(mice_sesh)
@@ -37,6 +41,10 @@ for j = 1:num_mice
    tdiff_cell{j} = make_timediff_mat(mice_sesh{j});
    % ID sessions that are day_lag apart 
    tdiff_bool = feval(compfun, tdiff_cell{j}, day_lag);
+   % Get hand_check_mat to grab only good sessions
+   [~, good_reg_mat_hand] = alt_hand_reg_order_check(mice_sesh{j});
+   % reject any sessions not meeting the hand check
+   tdiff_bool(good_reg_mat_hand == 0) = false;
    [a,b] = find(tdiff_bool);
    good_seshs{j} = [a,b];
 end
@@ -50,7 +58,7 @@ for j = 1:num_mice
     for k = 1:size(seshs_use,1)
         [~, stay_prop, coactive_prop, cat_names] = alt_stability_v_cat(...
             sesh_temp(seshs_use(k,1)), sesh_temp(seshs_use(k,2)), ...
-            'plot_flag',false);
+            'plot_flag', false, 'PFname', PFname);
         stay_prop_all{j} = cat(1,stay_prop_all{j},stay_prop);
         coactive_prop_all{j} = cat(1,coactive_prop_all{j},coactive_prop);
     end
