ee50d37abb64418ba6582f9c0a89510954a3c3b6
nkinsky <kinsky@bu.edu>
2018-10-12 07:10:37 -0400

    support for free_only and all other trial type comparisons

--- GCamp/Project specific code/alternation/alt_stab_v_cat_batch.m
+++ GCamp/Project specific code/alternation/alt_stab_v_cat_batch.m
@@ -1,5 +1,5 @@
 function [ stay_prop_all, coactive_prop_all, cat_names ] = ...
-    alt_stab_v_cat_batch(day_lag, comp_type, mice_sesh, PFname, matchER )
+    alt_stab_v_cat_batch(day_lag, comp_type, mice_sesh, PFname, matchER, trial_type )
 %  [stay_prop, coactive_prop, cat_names ] = alt_stab_v_cat_batch(...
 %       day_lag, comp_type, mouse1, mouse2,... )
 %   Batch function for getting splitter stability for all mice at the
@@ -18,11 +18,17 @@ function [ stay_prop_all, coactive_prop_all, cat_names ] = ...
 %
 %   match_ER: set to true to roughly match median event rates between all
 %   groups of neurons. default = false
+%
+%   trial_type: types of trials to compare. 'free_only' (default),
+%   'forced_only', 'all'
 
-if nargin < 5
-    matchER = false; % Don't match event-rates between splitters and other cells by default
-    if nargin < 4
-        PFname = 'Placefields_cm1.mat';
+if nargin < 6
+    trial_type = 'free_only';
+    if nargin < 5
+        matchER = false; % Don't match event-rates between splitters and other cells by default
+        if nargin < 4
+            PFname = 'Placefields_cm1.mat';
+        end
     end
 end
 
@@ -45,6 +51,25 @@ switch comp_type
         error('Must specify either ''exact'' or ''le'' for 2nd arg')
 end
 
+%% Get appropriate trial types and create boolean for selecting them in next step
+for j = 1:num_mice
+    [loop_bool, forced_bool] = alt_id_sesh_type(mice_sesh{j});
+    if ismember(trial_type, {'all', 'forced_only', 'free_only', 'no_loop'})
+        switch trial_type
+            case 'all'
+                good_bool{j} = true(size(loop_bool));
+            case 'forced_only'
+                good_bool{j} = forced_bool;
+            case 'free_only'
+                good_bool{j} = ~forced_bool & ~loop_bool;
+            case 'no_loop'
+                good_bool{j} = ~loop_bool;
+            otherwise
+                error('comp_type variable value is invalid')
+        end
+    end
+end
+
 %% Get day lag between all sessions
 tdiff_cell = cell(num_mice,1);
 good_seshs = cell(num_mice,1);
@@ -52,6 +77,8 @@ for j = 1:num_mice
    tdiff_cell{j} = make_timediff_mat(mice_sesh{j});
    % ID sessions that are exactly day_lag apart (or <= if 'le' specified)
    tdiff_bool = feval(compfun, tdiff_cell{j}, day_lag);
+   % Make sure you only include the appropriate session types
+   tdiff_bool = tdiff_bool & good_bool{j}.*good_bool{j}';
    % Get hand_check_mat to grab only good sessions
    [~, good_reg_mat_hand] = alt_hand_reg_order_check(mice_sesh{j});
    % reject any sessions not meeting the hand check
