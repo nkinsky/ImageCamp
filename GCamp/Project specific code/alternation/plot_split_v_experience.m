1bc5303334d12bcbfc21302ee58ac022e90ee4bc
nkinsky <kinsky@bu.edu>
2018-12-26 11:50:38 -0500

    initial commit

---
+++ GCamp/Project specific code/alternation/plot_split_v_experience.m
@@ -0,0 +1,8 @@
+function [] = plot_split_v_experience(sessions)
+% plot_split_v_experience(sessions)
+%   Plots splittiness metrics (deltamax and 1-p) versus # days of
+%   experience to see if splittiness emerges as a function of experience.
+outputArg1 = inputArg1;
+outputArg2 = inputArg2;
+end
+
